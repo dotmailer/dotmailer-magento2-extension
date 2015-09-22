f3a5be35257aa5f100313762701589466929460e
cdiacon <cdiacon@gmail.com>
2015-09-22 10:39:03 +0100

    add created at time

--- Model/Campaign.php
+++ Model/Campaign.php
@@ -33,6 +33,29 @@ class Campaign extends \Magento\Framework\Model\AbstractModel
 	//error messages
 	const SEND_EMAIL_CONTACT_ID_MISSING = 'Error : missing contact id - will try later to send ';
 
+
+
+	protected $_dateTime;
+
+	/**
+	 * @param \Magento\Framework\Model\Context $context
+	 * @param \Magento\Framework\Registry $registry
+	 * @param \Magento\Framework\Stdlib\DateTime $dateTime
+	 * @param \Magento\Framework\Model\Resource\AbstractResource $resource
+	 * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+	 * @param array $data
+	 */
+	public function __construct(
+		\Magento\Framework\Model\Context $context,
+		\Magento\Framework\Registry $registry,
+		\Magento\Framework\Stdlib\DateTime $dateTime,
+		\Magento\Framework\Model\Resource\AbstractResource $resource = null,
+		\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+		array $data = []
+	) {
+		$this->_dateTime = $dateTime;
+		parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+	}
 	/**
 	 * constructor
 	 */
@@ -60,6 +83,21 @@ class Campaign extends \Magento\Framework\Model\AbstractModel
 			     ->setStoreId($storeId);
 		}
 
+		return $this;
+	}
+
+	/**
+	 * Prepare data to be saved to database
+	 *
+	 * @return $this
+	 */
+	public function beforeSave()
+	{
+		parent::beforeSave();
+		if ($this->isObjectNew()) {
+			$this->setCreatedAt($this->_dateTime->formatDate(true));
+		}
+		$this->setUpdatedAt($this->_dateTime->formatDate(true));
 		return $this;
 	}
 
