8a17bd8e668910a8a5abf66dca33088ca4b79764
cdiacon <cdiacon@gmail.com>
2015-09-22 16:03:35 +0100

    #34 yes/no colors

--- Block/Adminhtml/Column/Renderer/Imported.php
+++ Block/Adminhtml/Column/Renderer/Imported.php
@@ -10,18 +10,9 @@ class Imported extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\Abstra
 	 *
 	 * @return string
 	 */
-	public function rendder(\Magento\Framework\DataObject $row)
-	{
-		$html = "<script type='application/javascript'>
-                    function visitPage(url){
-                        document.location.href = url;
-                    }
-                </script>";
-		return $html;
-	}
 	public function render(\Magento\Framework\DataObject $row)
 	{
-		return '<img style="padding-top:2px" '.(($this->_getValue($row)=='1' || $this->_getValue($row)==true) ? 'src="'.$this->getSkinUrl('images/success_msg_icon.gif').'" alt="YES" ' :   'src="'.
-			$this->getSkinUrl('images/error_msg_icon.gif').'" alt="NO" ').'>';
+		return '<div '. (($this->_getValue($row)=='1' || $this->_getValue($row)==true) ? 'class="dotmailer-success"  ' :
+			'class="dotmailer-error"  ').'>  </div>';
 	}
 }
--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -9,15 +9,22 @@ class Index extends \Magento\Framework\App\Action\Action
 {
 
 	protected $pageFactory;
-
+	protected $_localeDate;
+	protected $_scopeConfig;
 
 	/**
 	 * Pass arguments for dependency injection
 	 *
 	 * @param \Magento\Framework\App\Action\Context $context
 	 */
-	public function __construct(Context $context, PageFactory $pageFactory)
+	public function __construct(Context $context, PageFactory $pageFactory,
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfigInterface,
+		\Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
+
+	)
 	{
+		$this->_scopeConfig = $scopeConfigInterface;
+		$this->_localeDate = $localeDate;
 		$this->pageFactory = $pageFactory;
 		return parent::__construct($context);
 	}
@@ -27,6 +34,23 @@ class Index extends \Magento\Framework\App\Action\Action
 	 */
 	public function execute()
 	{
+
+		$fromTime = $this->_localeDate->date();
+		$interval = new \DateInterval("PT15M");
+		$fromTime->sub($interval);
+
+		$fromTime->getTimestamp();
+
+		$date = $fromTime->format('Y-m-d H:i:s');
+
+		$date = $this->_localeDate->date(null, null, false)->format('Y-m-d H:i:s');
+
+		var_dump($date);die;
+
+//		$dateStart = new \Datetime();
+//		$dateStart->setTimezone(new \DateTimeZone($timezoneLocal));
+//		$dateEnd->setTimezone(new \DateTimeZone($timezoneLocal));
+
 		//$model = $this->_objectManager->create('Dotdigitalgroup\Email\Model\Cron')->contactSync();
 		//$model = $this->_objectManager->create('Dotdigitalgroup\Email\Model\Cron')->emailImporter();
 		var_dump(__METHOD__);
