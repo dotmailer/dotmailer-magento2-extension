0f5bfcf9cfe93a4f388cc6713ec781cba102d207
Adeel Qamar <adeel.qamar@dotmailer.com>
2015-11-15 21:31:09 +0100

    Dynamic Content Styling #55

--- Block/Adminhtml/Config/Configuration/Colorpicker.php
+++ Block/Adminhtml/Config/Configuration/Colorpicker.php
@@ -15,14 +15,8 @@ class Colorpicker extends \Magento\Config\Block\System\Config\Form\Field
 		parent::__construct($context);
 	}
 
-    protected function _gedddtElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        // Include Procolor library JS file
-        //$html = '<script type="text/javascript" src="' . $jsPath . 'js/procolor-1.0/procolor.compressed.js' .'"></script>';
-        $html = '<script type="text/javascript" src="procolor.compressed.js"></script>';
-
-//$html = '<script type="text/javascript">ProColor.prototype.attachButton("connector_configuration_dynamic_content_style_font_color", { imgPath:"http://magentotwo.dev/admin/admin/index/index/key/a35d85a33acddedae2753bf2b47ba63cbc8c768fc1a6740819ceabf2a0691af8/connector/procolor-1.0/img/procolor_win_", showInField: true });</script><script type="text/javascript">';
-
         // Use Varien text element as a basis
         $input = $this->_text;
 
@@ -32,46 +26,35 @@ class Colorpicker extends \Magento\Config\Block\System\Config\Form\Field
             ->setValue($element->getValue())
             ->setHtmlId($element->getHtmlId())
             ->setName($element->getName())
-            ->setStyle('width: 60px') // Update style in order to shrink width
-            ->addClass('validate-hex'); // Add some Prototype validation to make sure color code is correct
+            ->setStyle('width: 60px'); // Update style in order to shrink width
 
         // Inject updated Varien text element HTML in our current HTML
-        $html .= $input->getHtml();
+        $html = $input->getHtml();
 
-        // Inject Procolor JS code to display color picker
-        $html .= $this->_getProcolorJs($element->getHtmlId());
-
-        // Inject Prototype validation
-        $html .= $this->_addHexValidator();
+        // Inject JS code to display color picker
+        $html .= $this->_getJs($element->getHtmlId());
 
         return $html;
     }
 
     /**
-     * Procolor JS code to display color picker
+     * JS code to display color picker
      *
-     * @see http://procolor.sourceforge.net/examples.php
+     * @see https://github.com/mrgrain/colpick
      * @param string $htmlId
      * @return string
      */
-    protected function _getProcolorJs($htmlId)
+    protected function _getJs($htmlId)
     {
-	    $this->_logger->debug('<script type="text/javascript">ProColor.prototype.attachButton(\'' . $htmlId . '\', { imgPath:\'' . $this->_urlBuilder->getUrl() . 'connector/procolor-1.0/' . 'img/procolor_win_\', showInField: true });</script>');
-        return '<script type="text/javascript">ProColor.prototype.attachButton(\'' . $htmlId . '\', { imgPath:\'' . $this->_urlBuilder->getUrl() . 'connector/procolor-1.0/' . 'img/procolor_win_\', showInField: true });</script>';
-    }
-
-    /**
-     * Prototype validation
-     *
-     * @return string
-     */
-    protected function _addHexValidator()
-    {
-        return
-            '<script type="text/javascript">
-                Validation.add(\'validate-hex\', \'' . 'Please enter a valid hex color code' . '\', function(v) {
-                    return /^#(?:[0-9a-fA-F]{3}){1,2}$/.test(v);
-                });
-            </script>';
+        return '<script type="text/javascript">
+                    require([\'jquery\', \'domReady!\'], function($){
+                            $(\'' . '#'.$htmlId . '\').colpick({
+                                onChange:function(hsb,hex,rgb,el,bySetColor)
+                                {
+                                    $(el).val(\'#\'+hex);
+                                }
+                            });
+                        });
+                </script>';
     }
 }
