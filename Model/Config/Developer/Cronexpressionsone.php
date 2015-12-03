9a8c2221bcad3692cd7138ce4d71baf434f63e34
Adeel Qamar <adeel.qamar@dotmailer.com>
2015-12-03 13:44:26 +0100

    Cron timing options #42

---
+++ Model/Config/Developer/Cronexpressionsone.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Model\Config\Developer;
+
+class Cronexpressionsone
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => '*/5 * * * *', 'label' => 'Every 5 Minutes'),
+            array('value' => '*/10 * * * *', 'label' => 'Every 10 Minutes'),
+            array('value' => '*/15 * * * *', 'label' => 'Every 15 Minutes')
+        );
+    }
+}
