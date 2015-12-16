75228b3196495760f95c970ffa7e1eb832e0a80c
cdiacon <cdiacon@gmail.com>
2015-12-16 10:05:08 +0000

    refactor coupon code block

--- Block/Coupon.php
+++ Block/Coupon.php
@@ -8,8 +8,15 @@ class Coupon extends \Magento\Framework\View\Element\Template
 	public $scopeManager;
 	public $objectManager;
 
+	protected $_ruleFactory;
+	protected $_massGeneratorFactory;
+	protected $_couponFactory;
+
 
 	public function __construct(
+		\Magento\Salesrule\Model\RuleFactory $ruleFactory,
+		\Magento\SalesRule\Model\Coupon\MassgeneratorFactory $massgeneratorFactory,
+		\Magento\Salesrule\Model\CouponFactory $couponFactory,
 		\Magento\Framework\View\Element\Template\Context $context,
 		\Dotdigitalgroup\Email\Helper\Data $helper,
 		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
@@ -19,51 +26,55 @@ class Coupon extends \Magento\Framework\View\Element\Template
 	{
 		parent::__construct( $context, $data );
 		$this->helper = $helper;
+		$this->_ruleFactory = $ruleFactory;
+		$this->_couponFactory = $couponFactory;
+		$this->_massGeneratorFactory = $massgeneratorFactory;
 		$this->scopeManager = $scopeConfig;
 		$this->storeManager = $this->_storeManager;
 		$this->objectManager = $objectManagerInterface;
 	}
-    /**
+	/**
 	 * Generates the coupon code based on the code id.
 	 * @return bool
 	 */
-    public function generateCoupon()
-    {
-        $params = $this->getRequest()->getParams();
-        if (!isset($params['id']) || !isset($params['code'])){
-            //throw new Exception('Coupon no id or code is set');
-            $this->helper->log('Coupon no id or code is set');
-            return false;
-        }
-        //coupon rule id
-        $couponCodeId = $params['id'];
+	public function generateCoupon()
+	{
+		$params = $this->getRequest()->getParams();
+		//check for param code and id
+		if (!isset($params['id']) || !isset($params['code'])){
+			$this->helper->log('Coupon no id or code is set');
+			return false;
+		}
+		//coupon rule id
+		$couponCodeId = $params['id'];
 
-        if ($couponCodeId) {
+		if ($couponCodeId) {
 
-            $rule = $this->objectManager->create('Magento\Salesrule\Model\Rule')->load($couponCodeId);
-            $generator = $this->objectManager->create('Magento\SalesRule\Model\Coupon\Massgenerator');
-	        $generator->setFormat( \Magento\SalesRule\Helper\Coupon::COUPON_FORMAT_ALPHANUMERIC );
-            $generator->setRuleId($couponCodeId);
-            $generator->setUsesPerCoupon(1);
-            $generator->setDash(3);
-            $generator->setLength(9);
-            $generator->setPrefix('');
-            $generator->setSuffix('');
-            //set the generation settings
-            $rule->setCouponCodeGenerator($generator);
-            $rule->setCouponType(\Magento\SalesRule\Model\Rule::COUPON_TYPE_AUTO);
-            //generate the coupon
-            $coupon = $rule->acquireCoupon();
-            $couponCode = $coupon->getCode();
-            //save the type of coupon
+			$rule = $this->_ruleFactory->create()
+			                           ->load($couponCodeId);
+			$generator = $this->_massGeneratorFactory->create();
+			$generator->setFormat( \Magento\SalesRule\Helper\Coupon::COUPON_FORMAT_ALPHANUMERIC );
+			$generator->setRuleId($couponCodeId);
+			$generator->setUsesPerCoupon(1);
+			$generator->setDash(3);
+			$generator->setLength(9);
+			$generator->setPrefix('');
+			$generator->setSuffix('');
+			//set the generation settings
+			$rule->setCouponCodeGenerator($generator);
+			$rule->setCouponType(\Magento\SalesRule\Model\Rule::COUPON_TYPE_AUTO);
+			//generate the coupon
+			$coupon = $rule->acquireCoupon();
+			$couponCode = $coupon->getCode();
+			//save the type of coupon
 
-            $couponModel = $this->objectManager->create('Magento\Salesrule\Model\Coupon')->loadByCode($couponCode);
-            $couponModel->setType(\Magento\SalesRule\Model\Rule::COUPON_TYPE_NO_COUPON);
-            $couponModel->save();
+			$couponModel = $this->_couponFactory->create()
+			                                    ->loadByCode($couponCode);
+			$couponModel->setType(\Magento\SalesRule\Model\Rule::COUPON_TYPE_NO_COUPON);
+			$couponModel->save();
 
-            return $couponCode;
-        }
-        return false;
-    }
-
+			return $couponCode;
+		}
+		return false;
+	}
 }
