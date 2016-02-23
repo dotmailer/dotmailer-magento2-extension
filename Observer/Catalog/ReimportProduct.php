5b46ee55ab9d03ececc56eb7af9cc186632d8ba8
cdiacon <cdiacon@gmail.com>
2016-02-23 11:30:20 +0000

    Code sniffer for M2 #106 psr1/2

--- Observer/Catalog/ReimportProduct.php
+++ Observer/Catalog/ReimportProduct.php
@@ -1,88 +1,100 @@
 <?php
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+
+
 namespace Dotdigitalgroup\Email\Observer\Catalog;
 
 class ReimportProduct implements \Magento\Framework\Event\ObserverInterface
 {
 
-	protected $_helper;
-	protected $_registry;
-	protected $_logger;
-	protected $_scopeConfig;
-	protected $_storeManager;
-	protected $_catalogFactory;
-	protected $_catalogCollection;
-	protected $_proccessorFactory;
-	protected $_connectorCatalogFactory;
-	protected $_connectorContactFactory;
+    protected $_helper;
+    protected $_registry;
+    protected $_logger;
+    protected $_scopeConfig;
+    protected $_storeManager;
+    protected $_catalogFactory;
+    protected $_catalogCollection;
+    protected $_proccessorFactory;
+    protected $_connectorCatalogFactory;
+    protected $_connectorContactFactory;
 
-	public function __construct(
-		\Dotdigitalgroup\Email\Model\Resource\ContactFactory $connectorContactFactory,
-		\Dotdigitalgroup\Email\Model\Resource\CatalogFactory $connectorCatalogFactory,
-		\Dotdigitalgroup\Email\Model\ProccessorFactory $proccessorFactory,
-		\Dotdigitalgroup\Email\Model\CatalogFactory $catalogFactory,
-		\Dotdigitalgroup\Email\Model\Resource\Catalog\CollectionFactory $catalogCollectionFactory,
-		\Magento\Framework\Registry $registry,
-		\Dotdigitalgroup\Email\Helper\Data $data,
-		\Psr\Log\LoggerInterface $loggerInterface,
-		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-		\Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
-		\Magento\Framework\ObjectManagerInterface $objectManagerInterface
-	)
-	{
-		$this->_connectorContactFactory = $connectorContactFactory;
-		$this->_connectorCatalogFactory = $connectorCatalogFactory;
-		$this->_proccessorFactory = $proccessorFactory;
-		$this->_helper = $data;
-		$this->_registry = $registry;
-		$this->_logger = $loggerInterface;
-		$this->_scopeConfig = $scopeConfig;
-		$this->_catalogFactory = $catalogFactory;
-		$this->_catalogCollection = $catalogCollectionFactory;
-		$this->_storeManager = $storeManagerInterface;
-	}
+    /**
+     * ReimportProduct constructor.
+     *
+     * @param \Dotdigitalgroup\Email\Model\Resource\ContactFactory            $connectorContactFactory
+     * @param \Dotdigitalgroup\Email\Model\Resource\CatalogFactory            $connectorCatalogFactory
+     * @param \Dotdigitalgroup\Email\Model\ProccessorFactory                  $proccessorFactory
+     * @param \Dotdigitalgroup\Email\Model\CatalogFactory                     $catalogFactory
+     * @param \Dotdigitalgroup\Email\Model\Resource\Catalog\CollectionFactory $catalogCollectionFactory
+     * @param \Magento\Framework\Registry                                     $registry
+     * @param \Dotdigitalgroup\Email\Helper\Data                              $data
+     * @param \Psr\Log\LoggerInterface                                        $loggerInterface
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface              $scopeConfig
+     * @param \Magento\Store\Model\StoreManagerInterface                      $storeManagerInterface
+     */
+    public function __construct(
+        \Dotdigitalgroup\Email\Model\Resource\ContactFactory $connectorContactFactory,
+        \Dotdigitalgroup\Email\Model\Resource\CatalogFactory $connectorCatalogFactory,
+        \Dotdigitalgroup\Email\Model\ProccessorFactory $proccessorFactory,
+        \Dotdigitalgroup\Email\Model\CatalogFactory $catalogFactory,
+        \Dotdigitalgroup\Email\Model\Resource\Catalog\CollectionFactory $catalogCollectionFactory,
+        \Magento\Framework\Registry $registry,
+        \Dotdigitalgroup\Email\Helper\Data $data,
+        \Psr\Log\LoggerInterface $loggerInterface,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Model\StoreManagerInterface $storeManagerInterface
+    ) {
+        $this->_connectorContactFactory = $connectorContactFactory;
+        $this->_connectorCatalogFactory = $connectorCatalogFactory;
+        $this->_proccessorFactory       = $proccessorFactory;
+        $this->_helper                  = $data;
+        $this->_registry                = $registry;
+        $this->_logger                  = $loggerInterface;
+        $this->_scopeConfig             = $scopeConfig;
+        $this->_catalogFactory          = $catalogFactory;
+        $this->_catalogCollection       = $catalogCollectionFactory;
+        $this->_storeManager            = $storeManagerInterface;
+    }
 
 
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
-		try{
-			$object = $observer->getEvent()->getDataObject();
-			$productId = $object->getId();
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        try {
+            $object    = $observer->getEvent()->getDataObject();
+            $productId = $object->getId();
 
-			if ($item = $this->_loadProduct($productId)){
-				if ($item->getImported())
-					$item->setModified(1)
-					     ->save();
-			}
-		}catch (\Exception $e){
-			$this->_helper->debug((string)$e, array());
-		}
-	}
+            if ($item = $this->_loadProduct($productId)) {
+                if ($item->getImported()) {
+                    $item->setModified(1)
+                        ->save();
+                }
+            }
+        } catch (\Exception $e) {
+            $this->_helper->debug((string)$e, array());
+        }
+    }
 
-	/**
-	 * load product. return item otherwise create item.
-	 *
-	 * @param $productId
-	 *
-	 * @return bool
-	 */
-	private function _loadProduct($productId)
-	{
-		$collection = $this->_catalogCollection->create()
-           ->addFieldToFilter('product_id', $productId)
-           ->setPageSize(1);
+    /**
+     * load product. return item otherwise create item.
+     *
+     * @param $productId
+     *
+     * @return bool
+     */
+    private function _loadProduct($productId)
+    {
+        $collection = $this->_catalogCollection->create()
+            ->addFieldToFilter('product_id', $productId)
+            ->setPageSize(1);
 
-		if ($collection->getSize()) {
-			return $collection->getFirstItem();
-		} else {
-			$this->_catalogFactory->create()
-                  ->setProductId($productId)
-                  ->save();
-		}
-		return false;
-	}
+        if ($collection->getSize()) {
+            return $collection->getFirstItem();
+        } else {
+            $this->_catalogFactory->create()
+                ->setProductId($productId)
+                ->save();
+        }
+
+        return false;
+    }
 
 }
--- Observer/Customer/CreateUpdateContact.php
+++ Observer/Customer/CreateUpdateContact.php
@@ -1,108 +1,126 @@
 <?php
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+
+
 namespace Dotdigitalgroup\Email\Observer\Customer;
 
 
 class CreateUpdateContact implements \Magento\Framework\Event\ObserverInterface
 {
-	protected $_helper;
-	protected $_registry;
-	protected $_logger;
-	protected $_storeManager;
-	protected $_objectManager;
-	protected $_wishlistFactory;
-	protected $_customerFactory;
-	protected $_contactFactory;
-	protected $_automationFactory;
-	protected $_proccessorFactory;
-	protected $_reviewFactory;
-	protected $_wishlist;
-	protected $_proccessor;
+
+    protected $_helper;
+    protected $_registry;
+    protected $_logger;
+    protected $_storeManager;
+    protected $_wishlistFactory;
+    protected $_customerFactory;
+    protected $_contactFactory;
+    protected $_automationFactory;
+    protected $_proccessorFactory;
+    protected $_reviewFactory;
+    protected $_wishlist;
+    protected $_proccessor;
 
 
-	public function __construct(
-		\Dotdigitalgroup\Email\Model\ReviewFactory $reviewFactory,
-		\Magento\Wishlist\Model\WishlistFactory $wishlist,
-		\Dotdigitalgroup\Email\Model\ProccessorFactory $proccessorFactory,
-		\Dotdigitalgroup\Email\Model\AutomationFactory $automationFactory,
-		\Dotdigitalgroup\Email\Model\ContactFactory $contactFactory,
-		\Magento\Customer\Model\CustomerFactory $customerFactory,
-		\Dotdigitalgroup\Email\Model\WishlistFactory $wishlistFactory,
-		\Magento\Framework\Registry $registry,
-		\Dotdigitalgroup\Email\Helper\Data $data,
-		\Psr\Log\LoggerInterface $loggerInterface,
-		\Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
-		\Magento\Framework\ObjectManagerInterface $objectManagerInterface,
-		\Dotdigitalgroup\Email\Model\Proccessor $proccessor
-	) {
-		$this->_reviewFactory = $reviewFactory;
-		$this->_wishlist = $wishlist;
-		$this->_contactFactory = $contactFactory;
-		$this->_proccessorFactory  = $proccessorFactory;
-		$this->_automationFactory = $automationFactory;
-		$this->_customerFactory = $customerFactory;
-		$this->_wishlistFactory = $wishlistFactory;
-		$this->_helper = $data;
-		$this->_logger = $loggerInterface;
-		$this->_storeManager = $storeManagerInterface;
-		$this->_registry = $registry;
-		$this->_objectManager = $objectManagerInterface;
-		$this->_proccessor  = $proccessor;
-	}
+    /**
+     * CreateUpdateContact constructor.
+     *
+     * @param \Dotdigitalgroup\Email\Model\ReviewFactory     $reviewFactory
+     * @param \Magento\Wishlist\Model\WishlistFactory        $wishlist
+     * @param \Dotdigitalgroup\Email\Model\ProccessorFactory $proccessorFactory
+     * @param \Dotdigitalgroup\Email\Model\AutomationFactory $automationFactory
+     * @param \Dotdigitalgroup\Email\Model\ContactFactory    $contactFactory
+     * @param \Magento\Customer\Model\CustomerFactory        $customerFactory
+     * @param \Dotdigitalgroup\Email\Model\WishlistFactory   $wishlistFactory
+     * @param \Magento\Framework\Registry                    $registry
+     * @param \Dotdigitalgroup\Email\Helper\Data             $data
+     * @param \Psr\Log\LoggerInterface                       $loggerInterface
+     * @param \Magento\Store\Model\StoreManagerInterface     $storeManagerInterface
+     * @param \Dotdigitalgroup\Email\Model\Proccessor        $proccessor
+     */
+    public function __construct(
+        \Dotdigitalgroup\Email\Model\ReviewFactory $reviewFactory,
+        \Magento\Wishlist\Model\WishlistFactory $wishlist,
+        \Dotdigitalgroup\Email\Model\ProccessorFactory $proccessorFactory,
+        \Dotdigitalgroup\Email\Model\AutomationFactory $automationFactory,
+        \Dotdigitalgroup\Email\Model\ContactFactory $contactFactory,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Dotdigitalgroup\Email\Model\WishlistFactory $wishlistFactory,
+        \Magento\Framework\Registry $registry,
+        \Dotdigitalgroup\Email\Helper\Data $data,
+        \Psr\Log\LoggerInterface $loggerInterface,
+        \Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
+        \Dotdigitalgroup\Email\Model\Proccessor $proccessor
+    ) {
+        $this->_reviewFactory     = $reviewFactory;
+        $this->_wishlist          = $wishlist;
+        $this->_contactFactory    = $contactFactory;
+        $this->_proccessorFactory = $proccessorFactory;
+        $this->_automationFactory = $automationFactory;
+        $this->_customerFactory   = $customerFactory;
+        $this->_wishlistFactory   = $wishlistFactory;
+        $this->_helper            = $data;
+        $this->_logger            = $loggerInterface;
+        $this->_storeManager      = $storeManagerInterface;
+        $this->_registry          = $registry;
+        $this->_proccessor        = $proccessor;
+    }
 
-	/**
-	 * If it's configured to capture on shipment - do this
-	 *
-	 * @param \Magento\Framework\Event\Observer $observer
-	 * @return $this
-	 */
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
-		$customer = $observer->getEvent()->getCustomer();
+    /**
+     * If it's configured to capture on shipment - do this
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     *
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $customer = $observer->getEvent()->getCustomer();
 
-		$email      = $customer->getEmail();
-		$websiteId  = $customer->getWebsiteId();
-		$customerId = $customer->getEntityId();
-		$isSubscribed = $customer->getIsSubscribed();
+        $email        = $customer->getEmail();
+        $websiteId    = $customer->getWebsiteId();
+        $customerId   = $customer->getEntityId();
+        $isSubscribed = $customer->getIsSubscribed();
 
-		try{
-			// fix for a multiple hit of the observer
-			$emailReg =  $this->_registry->registry($email . '_customer_save');
-			if ($emailReg){
-				return $this;
-			}
-			$this->_registry->register($email . '_customer_save', $email);
-			$emailBefore = $this->_customerFactory->create()->load($customer->getId())->getEmail();
-			$contactModel = $this->_contactFactory->create()->loadByCustomerEmail($emailBefore, $websiteId);
-			//email change detection
-			if ($email != $emailBefore) {
-				$this->_helper->log('email change detected : '  . $email . ', after : ' . $emailBefore .  ', website id : ' . $websiteId);
+        try {
+            // fix for a multiple hit of the observer
+            $emailReg = $this->_registry->registry($email . '_customer_save');
+            if ($emailReg) {
+                return $this;
+            }
+            $this->_registry->register($email . '_customer_save', $email);
+            $emailBefore  = $this->_customerFactory->create()
+                ->load($customer->getId())->getEmail();
+            $contactModel = $this->_contactFactory->create()
+                ->loadByCustomerEmail($emailBefore, $websiteId);
+            //email change detection
+            if ($email != $emailBefore) {
+                $this->_helper->log('email change detected : ' . $email
+                    . ', after : ' . $emailBefore . ', website id : '
+                    . $websiteId);
 
-				$data = array(
-					'emailBefore' => $emailBefore,
-					'email' => $email,
-					'isSubscribed' => $isSubscribed
-				);
-				$this->_proccessor->registerQueue(
-					\Dotdigitalgroup\Email\Model\Proccessor::IMPORT_TYPE_CONTACT_UPDATE,
-					$data,
-					\Dotdigitalgroup\Email\Model\Proccessor::MODE_CONTACT_EMAIL_UPDATE,
-					$websiteId
-				);
+                $data = array(
+                    'emailBefore'  => $emailBefore,
+                    'email'        => $email,
+                    'isSubscribed' => $isSubscribed
+                );
+                $this->_proccessor->registerQueue(
+                    \Dotdigitalgroup\Email\Model\Proccessor::IMPORT_TYPE_CONTACT_UPDATE,
+                    $data,
+                    \Dotdigitalgroup\Email\Model\Proccessor::MODE_CONTACT_EMAIL_UPDATE,
+                    $websiteId
+                );
 
-			}elseif (!$emailBefore) {
-				//for new contacts update email
-				$contactModel->setEmail( $email );
-			}
-			$contactModel->setEmailImported(\Dotdigitalgroup\Email\Model\Contact::EMAIL_CONTACT_NOT_IMPORTED)
-			             ->setCustomerId($customerId)
-			             ->save();
-		}catch(\Exception $e){
-			$this->_helper->debug((string)$e, array());
-		}
-		return $this;
-	}
+            } elseif ( ! $emailBefore) {
+                //for new contacts update email
+                $contactModel->setEmail($email);
+            }
+            $contactModel->setEmailImported(\Dotdigitalgroup\Email\Model\Contact::EMAIL_CONTACT_NOT_IMPORTED)
+                ->setCustomerId($customerId)
+                ->save();
+        } catch (\Exception $e) {
+            $this->_helper->debug((string)$e, array());
+        }
+
+        return $this;
+    }
 }
