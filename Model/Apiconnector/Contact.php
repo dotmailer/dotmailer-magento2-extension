d5498bed4c63c6eb14d961c21494b775c1cdffe5
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-06-09 12:56:45 +0200

    Create instance of class from factory outside of __construct #57

--- Model/Apiconnector/Contact.php
+++ Model/Apiconnector/Contact.php
@@ -108,13 +108,11 @@ class Contact
         //email contact
         $this->_emailCustomer = $customerFactory;
         $this->_contactFactory = $contactFactory;
-        $this->_customerCollection = $customerCollectionFactory->create();
-        $this->_customerCollection->addAttributeToSelect('*');
+        $this->_customerCollection = $customerCollectionFactory;
         //email contact collection
-        $this->_contactCollection = $contactCollectionFactory->create();
-        $this->_contactCollection->addFieldToSelect('*');
+        $this->_contactCollection = $contactCollectionFactory;
         //newsletter subscriber
-        $this->_subscriberFactory = $subscriberFactory->create();
+        $this->_subscriberFactory = $subscriberFactory;
     }
 
     /**
@@ -195,7 +193,8 @@ class Contact
         $connection = $this->_resource->getConnection();
 
         //contacts ready for website
-        $contacts = $this->_contactCollection
+        $contacts = $this->_contactCollection->create()
+            ->addFieldToSelect('*')
             ->addFieldToFilter('email_imported', ['null' => true])
             ->addFieldToFilter('customer_id', ['neq' => '0'])
             ->addFieldToFilter('website_id', $website->getId())
@@ -416,7 +415,7 @@ class Contact
             $contactModel->setEmailImported(
                 \Dotdigitalgroup\Email\Model\Contact::EMAIL_CONTACT_IMPORTED
             );
-            $subscriber = $this->_subscriberFactory->loadByEmail(
+            $subscriber = $this->_subscriberFactory->create()->loadByEmail(
                 $customer->getEmail()
             );
             if ($subscriber->isSubscribed()) {
@@ -462,7 +461,9 @@ class Contact
      */
     protected function _getCustomerCollection($customerIds, $websiteId = 0)
     {
-        $customerCollection = $this->_customerCollection->addNameToSelect()
+        $customerCollection = $this->_customerCollection->create()
+            ->addAttributeToSelect('*')
+            ->addNameToSelect()
             ->joinAttribute(
                 'billing_street', 'customer_address/street', 'default_billing',
                 null, 'left'
