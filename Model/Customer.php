58010a7df252d55d59ffaaf2e905bab94f325e60
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-06-30 12:03:26 +0200

    New customer emails still sending when disabled #85

---
+++ Model/Customer.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Model;
+
+class Customer extends \Magento\Customer\Model\AccountManagement
+{
+    private $_scopeConfig;
+
+    /**
+     * @param \Magento\Customer\Model\CustomerFactory $customerFactory
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Math\Random $mathRandom
+     * @param \Magento\Customer\Model\Metadata\Validator $validator
+     * @param \Magento\Customer\Api\Data\ValidationResultsInterfaceFactory $validationResultsDataFactory
+     * @param \Magento\Customer\Api\AddressRepositoryInterface $addressRepository
+     * @param \Magento\Customer\Api\CustomerMetadataInterface $customerMetadataService
+     * @param \Magento\Customer\Model\CustomerRegistry $customerRegistry
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Encryption\EncryptorInterface $encryptor
+     * @param \Magento\Customer\Model\Config\Share $configShare
+     * @param \Magento\Framework\Stdlib\StringUtils $stringHelper
+     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
+     * @param \Magento\Framework\Reflection\DataObjectProcessor $dataProcessor
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Customer\Helper\View $customerViewHelper
+     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param \Magento\Customer\Model\Customer $customerModel
+     * @param \Magento\Framework\DataObjectFactory $objectFactory
+     * @param \Magento\Framework\Api\ExtensibleDataObjectConverter $extensibleDataObjectConverter
+     */
+    public function __construct(
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Math\Random $mathRandom,
+        \Magento\Customer\Model\Metadata\Validator $validator,
+        \Magento\Customer\Api\Data\ValidationResultsInterfaceFactory $validationResultsDataFactory,
+        \Magento\Customer\Api\AddressRepositoryInterface $addressRepository,
+        \Magento\Customer\Api\CustomerMetadataInterface $customerMetadataService,
+        \Magento\Customer\Model\CustomerRegistry $customerRegistry,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor,
+        \Magento\Customer\Model\Config\Share $configShare,
+        \Magento\Framework\Stdlib\StringUtils $stringHelper,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Framework\Reflection\DataObjectProcessor $dataProcessor,
+        \Magento\Framework\Registry $registry,
+        \Magento\Customer\Helper\View $customerViewHelper,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        \Magento\Customer\Model\Customer $customerModel,
+        \Magento\Framework\DataObjectFactory $objectFactory,
+        \Magento\Framework\Api\ExtensibleDataObjectConverter $extensibleDataObjectConverter
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+        parent::__construct($customerFactory, $eventManager, $storeManager, $mathRandom, $validator,
+            $validationResultsDataFactory, $addressRepository, $customerMetadataService, $customerRegistry, $logger,
+            $encryptor, $configShare, $stringHelper, $customerRepository, $scopeConfig, $transportBuilder,
+            $dataProcessor, $registry, $customerViewHelper, $dateTime, $customerModel, $objectFactory,
+            $extensibleDataObjectConverter);
+    }
+
+    /**
+     * Send email with new account related information
+     *
+     * @param \Magento\Customer\Api\Data\CustomerInterface $customer
+     * @param string $type
+     * @param string $backUrl
+     * @param string $storeId
+     * @param string $sendemailStoreId
+     * @return $this
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @deprecated
+     */
+    protected function sendNewAccountEmail(
+        $customer,
+        $type = self::NEW_ACCOUNT_EMAIL_REGISTERED,
+        $backUrl = '',
+        $storeId = '0',
+        $sendemailStoreId = null
+    ) {
+        if ($this->_scopeConfig->isSetFlag(
+            \Dotdigitalgroup\Email\Helper\Config::XML_PATH_CONNECTOR_DISABLE_CUSTOMER_SUCCESS,
+            'store', $storeId)
+        ) {
+            return $this;
+        } else {
+            return parent::sendNewAccountEmail($customer, $type, $backUrl, $storeId, $sendemailStoreId);
+        }
+    }
+}
