4856cc04dff5fba10bc3e115bea76c5f8a9fe443
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-09-09 11:11:02 +0100

    Review/improve process of finding guests - M2

--- Model/ResourceModel/Contact.php
+++ Model/ResourceModel/Contact.php
@@ -115,4 +115,14 @@ class Contact extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
             throw new \Magento\Framework\Exception\LocalizedException(__($e->getMessage()));
         }
     }
+
+    public function insert($data)
+    {
+        try {
+            $write = $this->getConnection();
+            $write->insertMultiple($this->getMainTable(), $data);
+        } catch (\Exception $e) {
+            throw new \Magento\Framework\Exception\LocalizedException(__($e->getMessage()));
+        }
+    }
 }
