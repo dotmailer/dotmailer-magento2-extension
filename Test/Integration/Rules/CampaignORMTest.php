0689495fd8cfb6a064db0042d4307d4f1ae13424
cdiacon <cdiacon@gmail.com>
2016-09-27 14:37:32 +0100

    add integration tests

---
+++ Test/Integration/Rules/CampaignORMTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Model;
+
+use Dotdigitalgroup\Email\Model\ResourceModel\Campaign\Collection as CampaignCollection;
+use Magento\Framework\App\ResourceConnection;
+use Magento\TestFramework\ObjectManager;
+
+class CampaignORMTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return Campaign
+     */
+    private function createCampaignModel()
+    {
+        return ObjectManager::getInstance()->create(Campaign::class);
+    }
+
+    /**
+     * @param int|string $customerId
+     * @param string $email
+     * @return Campaign
+     */
+    private function createCampaign($customerId, $email)
+    {
+        $model = $this->createCampaignModel();
+        $model->setData('email', $email);
+        $model->setData('customer_id', $customerId);
+        $model->setData('message', 'Test Message');
+        $model->getResource()->save($model);
+        return $model;
+    }
+
+    private function assertCollectionContains(Campaign $campaign, CampaignCollection $collection)
+    {
+        $message = sprintf('Expected campaign with ID "%s" not found in collection', $campaign->getId());
+        $this->assertContains($campaign->getId(), array_keys($collection->getItems()), $message);
+    }
+
+    protected function setUp()
+    {
+        /** @var CampaignCollection $collection */
+        $collection = ObjectManager::getInstance()->create(CampaignCollection::class);
+        $collection->walk(function (Campaign $campaign) {
+            $campaign->getResource()->delete($campaign);
+        });
+    }
+
+    public function testCampaignTableExists()
+    {
+        /** @var ResourceConnection $resource */
+        $resource = ObjectManager::getInstance()->get(ResourceConnection::class);
+        $tableName = $resource->getTableName('email_campaign');
+        $this->assertTrue($resource->getConnection('default')->isTableExists($tableName));
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     */
+    public function testModelAndResourceModelORMConfiguration()
+    {
+        $model = $this->createCampaign('1', 'test@example.com');
+
+        $loadedModel = $this->createCampaignModel();
+        $loadedModel->getResource()->load($loadedModel, $model->getId());
+
+        $this->assertNotNull($model->getId());
+        $this->assertSame($model->getData('email'), $loadedModel->getData('email'));
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     */
+    public function testCollectionORMConfiguration()
+    {
+        $campaignA = $this->createCampaign(1, 'customerA@example.com');
+        $campaignB = $this->createCampaign(2, 'customerB@example.com');
+
+        /** @var CampaignCollection $collection */
+        $collection = ObjectManager::getInstance()->create(CampaignCollection::class);
+        $collection->load();
+
+        $this->assertCollectionContains($campaignA, $collection);
+        $this->assertCollectionContains($campaignB, $collection);
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     */
+    public function testCanBeLoadedByQuoteId()
+    {
+        $storeId = 1;
+        $dummyQuoteId = 3;
+        $model = $this->createCampaign(1, 'foo@example.com');
+        $model->setData('store_id', $storeId);
+        $model->setData('quote_id', $dummyQuoteId);
+        $model->getResource()->save($model);
+
+        /** @var Campaign $emptyModel */
+        $emptyModel = ObjectManager::getInstance()->create(Campaign::class);
+        $loadedModel = $emptyModel->loadByQuoteId($dummyQuoteId, $storeId);
+
+        $this->assertSame($model->getId(), $loadedModel->getId());
+    }
+}
---
+++ Test/Integration/Rules/NewsletterTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Controller\Customer;
+
+use Dotdigitalgroup\Email\Helper\Data;
+use Dotdigitalgroup\Email\Model\Apiconnector\Client;
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Data\Form\FormKey\Validator as FromKeyValidator;
+use Magento\Store\Model\Store;
+use Magento\TestFramework\ObjectManager;
+
+class NewsletterTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    public function testConnectorContactIdNotSetCausesRedirect()
+    {
+        /** @var ObjectManager $objectManager */
+        $objectManager = ObjectManager::getInstance();
+
+        $mockFormKeyValidator = $this->getMockBuilder(FromKeyValidator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockFormKeyValidator->method('validate')->willReturn(true);
+        $objectManager->addSharedInstance($mockFormKeyValidator, FromKeyValidator::class);
+
+        $sessionMethods = array_merge(get_class_methods(CustomerSession::class), ['getConnectorContactId']);
+        $mockCustomerSession = $this->getMockBuilder(CustomerSession::class)
+            ->disableOriginalConstructor()
+            ->setMethods($sessionMethods)
+            ->getMock();
+
+        $mockCustomerSession->method('getConnectorContactId')->willReturn(false);
+        $objectManager->addSharedInstance($mockCustomerSession, CustomerSession::class);
+
+        $mockCustomerSession->expects($this->never())->method('getCustomer');
+
+        $this->dispatch('connector/customer/newsletter');
+
+        $this->assertRedirect($this->stringContains('/customer/account/'));
+    }
+
+    public function testContactDataFieldsAreUpdatedByEmail()
+    {
+        $this->markTestSkipped();
+        ///updatecontactdatafieldsbyemail called
+        /** @var ObjectManager  $objectManager */
+        $objectManager = ObjectManager::getInstance();
+
+        $mockFormKeyValidator = $this->getMockBuilder(FromKeyValidator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockFormKeyValidator->method('validate')->willReturn(true);
+        $objectManager->addSharedInstance($mockFormKeyValidator, FromKeyValidator::class);
+
+        $sessionMethods = array_merge(get_class_methods(CustomerSession::class), [
+            'getConnectorContactId',
+        ]);
+        /**
+         * Customer Session mock.
+         */
+        $mockCustomerSession = $this->getMockBuilder(CustomerSession::class)
+            ->disableOriginalConstructor()
+            ->setMethods($sessionMethods)
+            ->getMock();
+        $mockCustomerSession->method('getConnectorContactId')->willReturn('dummy123');
+        $mockCustomerSession->method('getCustomerId')->willReturn(1);
+        $objectManager->addSharedInstance($mockCustomerSession, CustomerSession::class);
+
+        /**
+         * Customer mock.
+         */
+        $customerMethods = array_merge(get_class_methods(Customer::class));
+        $mockCustomer = $this->getMockBuilder(Customer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /**
+         * Store mock.
+         */
+        $storeMethods = array_merge(get_class_methods(Store::class), ['getWebsite']);
+        $mockStore = $this->getMockBuilder(Store::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /**
+         * Helper mock.
+         */
+        $mockHelper = $this->getMockBuilder(Data::class)
+
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /**
+         * Client mock.
+         */
+        $mockClient = $this->getMockBuilder(Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        //\Dotdigitalgroup\Email\Helper\Data
+        $objectManager->addSharedInstance($mockHelper, Data::class);
+
+
+
+        $mockCustomerSession->method('getCustomer')->willReturn($mockCustomer);
+        $mockCustomer->method('getStore')->willReturn($mockStore);
+        $mockStore->method('getWebsite')->willReturn('0');
+        $mockHelper->method('isEnabled')->willReturn('1');
+        $mockHelper->method('getWebsiteApiClient')->willReturn($mockClient);
+        $mockClient->method('getContactById')->willReturn((object) ['id' => '111']);
+        $mockClient->method('setApiUsername')->willReturn('93465');
+        $mockClient->method('getDataFields')->willReturn([]);
+        $mockClient->method('setApipassword')->willReturn('pass');
+        $mockClient->method('getContactById')->willReturn((object) ['id' => '111']);
+        $mockClient->expects($this->atLeastOnce())->method('updateContactDatafieldsByEmail')->willReturn(true);
+
+
+        $this->getRequest()->setParam('data_fields', []);
+
+        $this->dispatch('connector/customer/newsletter');
+    }
+}
