236804bfb0a54470dabefccabe0d2cda85e40d85
Adeel Qamar <adeel.qamar@dotmailer.com>
2016-11-04 15:27:50 +0000

    Add mass delete to rules #134

--- Model/ResourceModel/Rules.php
+++ Model/ResourceModel/Rules.php
@@ -11,4 +11,26 @@ class Rules extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     {
         $this->_init('email_rules', 'id');
     }
+
+    /**
+     * Mass delete
+     *
+     * @param $data
+     * @return int|string
+     */
+    public function massDelete($data)
+    {
+        try {
+            $ids = '"' . implode('","', $data) . '"';
+            $conn = $this->getConnection();
+            $num = $conn->delete(
+                $this->getMainTable(),
+                "id IN ($ids)"
+            );
+
+            return $num;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
 }
