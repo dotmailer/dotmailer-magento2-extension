728acd4f7dca89931cce5889662fb4573b9b5298
Calin Diacon <calin.diacon@dotmailer.com>
2017-07-10 11:28:37 +0100

    fix compilation errors

--- Observer/Catalog/RemoveProduct.php
+++ Observer/Catalog/RemoveProduct.php
@@ -35,17 +35,13 @@ class RemoveProduct implements \Magento\Framework\Event\ObserverInterface
      * @var \Dotdigitalgroup\Email\Model\ImporterFactory
      */
     private $importerFactory;
-    /**
-     * @var \Dotdigitalgroup\Email\Model\ResourceModel\Catalog
-     */
-    private $catalogResource;
 
     /**
      * RemoveProduct constructor.
      *
-     * @param \Dotdigitalgroup\Email\Model\ResourceModel\Catalog                   $catalogResource
-     * @param \Dotdigitalgroup\Email\Model\ImporterFactory                         $importerFactory
-     * @param \Dotdigitalgroup\Email\Model\CatalogFactory                          $catalogFactory
+     * @param \Dotdigitalgroup\Email\Model\ResourceModel\Catalog $catalogResource
+     * @param \Dotdigitalgroup\Email\Model\ImporterFactory $importerFactory
+     * @param \Dotdigitalgroup\Email\Model\CatalogFactory $catalogFactory
      * @param \Dotdigitalgroup\Email\Model\ResourceModel\Catalog\CollectionFactory $catalogCollectionFactory
      * @param \Dotdigitalgroup\Email\Helper\Data $data
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
@@ -55,7 +51,6 @@ class RemoveProduct implements \Magento\Framework\Event\ObserverInterface
         \Dotdigitalgroup\Email\Model\ResourceModel\Catalog $catalogResource,
         \Dotdigitalgroup\Email\Model\ImporterFactory $importerFactory,
         \Dotdigitalgroup\Email\Model\CatalogFactory $catalogFactory,
-        \Dotdigitalgroup\Email\Model\ResourceModel\Catalog $catalogResource,
         \Dotdigitalgroup\Email\Model\ResourceModel\Catalog\CollectionFactory $catalogCollectionFactory,
         \Dotdigitalgroup\Email\Helper\Data $data,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
@@ -68,7 +63,6 @@ class RemoveProduct implements \Magento\Framework\Event\ObserverInterface
         $this->catalogResource = $catalogResource;
         $this->catalogCollection = $catalogCollectionFactory;
         $this->storeManager      = $storeManagerInterface;
-        $this->catalogResource   = $catalogResource;
     }
 
     /**
