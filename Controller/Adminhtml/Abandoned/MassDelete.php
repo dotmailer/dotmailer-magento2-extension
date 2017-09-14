3247aceba9f993b4cdda547c507157e7f8ae3c70
Calin Diacon <calin.diacon@dotmailer.com>
2017-09-14 16:35:35 +0100

    abandoned cart - report grid

---
+++ Controller/Adminhtml/Abandoned/MassDelete.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Controller\Adminhtml\Abandoned;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Ui\Component\MassAction\Filter;
+
+class MassDelete extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Dotdigitalgroup\Email\Model\ResourceModel\Abandoned
+     */
+    public $abandonedResource;
+
+    /**
+     * @var
+     */
+    public $abandonedCollection;
+
+    /**
+     * @var \Magento\Framework\Message\ManagerInterface
+     */
+    protected $messageManager;
+
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * @var \Dotdigitalgroup\Email\Model\ResourceModel\Automation
+     */
+    private $automationResource;
+
+    /**
+     * MassDelete constructor.
+     * @param \Dotdigitalgroup\Email\Model\ResourceModel\Abandoned $abandonedResource
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param Filter $filter
+     * @param \Dotdigitalgroup\Email\Model\ResourceModel\Abandoned\CollectionFactory $abandonedCollection
+     */
+    public function __construct(
+        \Dotdigitalgroup\Email\Model\ResourceModel\Abandoned $abandonedResource,
+        \Magento\Backend\App\Action\Context $context,
+        Filter $filter,
+        \Dotdigitalgroup\Email\Model\ResourceModel\Abandoned\CollectionFactory $abandonedCollection
+    ) {
+        $this->filter = $filter;
+        $this->abandonedCollection = $abandonedCollection->create();
+        $this->abandonedResource = $abandonedResource;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->abandonedCollection);
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection as $item) {
+            $this->abandonedResource->delete($item);
+        }
+
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        return $resultRedirect->setPath('*/*/');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Dotdigitalgroup_Email::abandoned');
+    }
+}
---
+++ Model/Abandoned.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Model;
+
+class Abandoned extends \Magento\Framework\Model\AbstractModel
+{
+    /**
+     * @var ResourceModel\Automation
+     */
+    private $automationResource;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime
+     */
+    private $dateTime;
+
+    /**
+     * @var \Dotdigitalgroup\Email\Helper\Data
+     */
+    private $helper;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * Abandoned constructor.
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->dateTime     = $dateTime;
+        parent::__construct(
+            $context,
+            $registry,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
+    /**
+     * Constructor.
+     *
+     * @return null
+     */
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init(\Dotdigitalgroup\Email\Model\ResourceModel\Abandoned::class);
+    }
+
+    /**
+     * Prepare data to be saved to database.
+     *
+     * @return $this
+     */
+    public function beforeSave()
+    {
+        parent::beforeSave();
+        if ($this->isObjectNew()) {
+            $this->setCreatedAt($this->dateTime->formatDate(true));
+        }
+        $this->setUpdatedAt($this->dateTime->formatDate(true));
+
+        return $this;
+    }
+}
