5b1fe066156155e0a49ad9c324d0c49bcc461922
adeelq <adeel.qamar@dotmailer.com>
2017-10-20 12:57:58 +0200

    Add logging to importer and catalog sync

--- Model/Importer.php
+++ Model/Importer.php
@@ -242,6 +242,12 @@ class Importer extends \Magento\Framework\Model\AbstractModel
             $this->helper->debug((string)$e, []);
         }
 
+        if (json_last_error_msg() != "No error") {
+            $jle = json_last_error_msg();
+            $format = "Json error ($jle) for Import type ($importType) / mode ($importMode) for website ($websiteId)";
+            $this->helper->log($format);
+        }
+
         return false;
     }
 
