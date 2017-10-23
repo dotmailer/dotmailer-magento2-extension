6b3eb9b3a9edd913143e297a7fe41e4cb4529398
Simon Letch <simon.letch@dotmailer.com>
2017-10-23 08:37:36 +0000

    Merged PR 4544: Merge copy-fixes to testing
    BUNDLE-343
    BUNDLE-335
    BUNDLE-334
    BUNDLE-333
    
    Copy fixes

--- Controller/Adminhtml/Run/Automapdatafields.php
+++ Controller/Adminhtml/Run/Automapdatafields.php
@@ -113,7 +113,7 @@ class Automapdatafields extends \Magento\Backend\App\AbstractAction
             if ($result['errors']) {
                 $this->messageManager->addNoticeMessage($result['message']);
             } else {
-                $this->messageManager->addSuccessMessage('All Datafields Created And Mapped.');
+                $this->messageManager->addSuccessMessage('All Data Fields Created And Mapped.');
             }
 
             //Clear config cache
