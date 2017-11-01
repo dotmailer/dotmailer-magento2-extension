9629d5ddd66245e9faf5bcc94bf71a7d235f4c6b
adeelq <adeel.qamar@dotmailer.com>
2017-11-01 11:03:17 +0100

    Remove curl options #57384

--- Model/Apiconnector/Client.php
+++ Model/Apiconnector/Client.php
@@ -1367,8 +1367,6 @@ class Client extends \Dotdigitalgroup\Email\Model\Apiconnector\Rest
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POST, count($params));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
