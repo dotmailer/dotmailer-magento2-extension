f555d41e9b87a239197d0df532a5af3584ae8a15
Calin Diacon <calin.diacon@dotmailer.com>
2017-11-27 11:58:42 +0000

    email templates - run button in admin for sync
    (cherry picked from commit d367954)

---
+++ Block/Adminhtml/Config/Developer/EmailTemplatesSync.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Block\Adminhtml\Config\Developer;
+
+class EmailTemplatesSync extends \Magento\Config\Block\System\Config\Form\Field
+{
+
+    /**
+     * @var string
+     */
+    public $buttonLabel = 'Run Now';
+
+    /**
+     * @param string $buttonLabel
+     *
+     * @return $this
+     */
+    public function setButtonLabel($buttonLabel)
+    {
+        $this->buttonLabel = $buttonLabel;
+
+        return $this;
+    }
+
+    /**
+     * Get the button and scripts contents.
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     *
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $url = $this->_urlBuilder->getUrl('dotdigitalgroup_email/run/templatesync');
+
+        return $this->getLayout()
+            ->createBlock(\Magento\Backend\Block\Widget\Button::class)
+            ->setType('button')
+            ->setLabel($this->buttonLabel)
+            ->setOnClick("window.location.href='" . $url . "'")
+            ->toHtml();
+    }
+}
