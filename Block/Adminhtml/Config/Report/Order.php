306722bae79dafb265ba2f19ae4cf62714759796
Simon Letch <simon.letch@dotmailer.com>
2018-07-26 10:15:06 +0000

    Merged PR 8921: Don't remove public methods in blocks. Deprecated instead, to be removed later.
    Don't remove public methods in blocks. Deprecated instead, to be removed later.

--- Block/Adminhtml/Config/Report/Order.php
+++ Block/Adminhtml/Config/Report/Order.php
@@ -2,8 +2,47 @@
 
 namespace Dotdigitalgroup\Email\Block\Adminhtml\Config\Report;
 
-class Order extends \Dotdigitalgroup\Email\Block\Adminhtml\Config\Report\Report
+class Order extends \Magento\Config\Block\System\Config\Form\Field
 {
+
+    /**
+     * @deprecated
+     *
+     * @var string
+     */
+    public $buttonLabel = 'Contact Report';
+
+    /**
+     * @deprecated
+     *
+     * @param string $buttonLabel
+     *
+     * @return $this
+     */
+    public function setButtonLabel($buttonLabel)
+    {
+        $this->buttonLabel = $buttonLabel;
+
+        return $this;
+    }
+
+    /**
+     * Set template to itself.
+     *
+     * @deprecated
+     *
+     * @return mixed
+     */
+    public function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if (!$this->getTemplate()) {
+            $this->setTemplate('system/config/reportlink.phtml');
+        }
+
+        return $this;
+    }
+
     /**
      * @return string
      */
@@ -12,5 +51,49 @@ class Order extends \Dotdigitalgroup\Email\Block\Adminhtml\Config\Report\Report
         return $this->getUrl(
             'dotdigitalgroup_email/order/index'
         );
+    }
+
+    /**
+     * Unset some non-related element parameters.
+     *
+     * @deprecated
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     *
+     * @return string
+     */
+    public function render(
+        \Magento\Framework\Data\Form\Element\AbstractElement $element
+    ) {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+
+        return parent::render($element);
+    }
+
+    /**
+     * Get the button and scripts contents.
+     *
+     * @deprecated
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     *
+     * @return string
+     */
+    public function _getElementHtml(
+        \Magento\Framework\Data\Form\Element\AbstractElement $element
+    ) {
+        $originalData = $element->getOriginalData();
+        $buttonLabel = !empty($originalData['button_label'])
+            ? $originalData['button_label'] : $this->buttonLabel;
+        $url = $this->_urlBuilder->getUrl('dotdigitalgroup_email/addressbook/save');
+        $this->addData(
+            [
+                'button_label' => $buttonLabel,
+                'html_id' => $element->getHtmlId(),
+                'ajax_url' => $url,
+            ]
+        );
+
+        return $this->_toHtml();
     }
 }
