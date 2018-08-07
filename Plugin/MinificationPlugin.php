3a5facd244073393280bae5c09c864fecfd07356
Adeel Qamar <Adeel.Qamar@dotmailer.com>
2018-08-07 07:55:11 +0000

    Merged PR 9007: prevent minification of external resources #73814
    prevent minification of external resources #73814
    
    Related work items: #73814

---
+++ Plugin/MinificationPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Plugin;
+
+/**
+ * Class MinificationPlugin
+ *
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+class MinificationPlugin
+{
+    /**
+     * Exclude external js from minification
+     *
+     * @param \Magento\Framework\View\Asset\Minification $subject
+     * @param callable $proceed
+     * @param string $contentType
+     *
+     * @return array
+     */
+    public function aroundGetExcludes(
+        \Magento\Framework\View\Asset\Minification $subject,
+        callable $proceed,
+        $contentType
+    ) {
+        $result = $proceed($contentType);
+
+        //Content type can be css or js
+        if ($contentType == 'js') {
+            $result[] = 'trackedlink.net/_dmpt.js';
+            $result[] = 'trackedlink.net/_dmmpt.js';
+        }
+
+        return $result;
+    }
+}
