d155c7583a169f43472c8d3e0a773dc7ed032339
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2019-07-31 16:08:16 +0000

    Merged PR 14975: Use Monolog for logging
    ## What's being changed
    
    Logging.
    
    ## Why it's being changed
    
    As correctly observed by community developers, we had a non-standard approach to logging connector-specific messages to our connector.log log file. This PR adopts Monolog (as per Magento core) to remedy this.
    
    Our new `Logger\Logger` class has been injected into `\Helper\FileHelper` as per the previous implementation, and we have two handlers for INFO and DEBUG log types. We still log by calling `$this->helper->log($message)` and `$this->helper->debug($message, $dataArray)` as before.
    
    See sample connector.log output: https://gist.github.com/sta1r/4c1bfa20ce003fb5207e1c5e6fdcd792
    
    Related work items: #88217

--- Helper/File.php
+++ Helper/File.php
@@ -29,11 +29,6 @@ class File
     private $enclosure;
 
     /**
-     * @var string
-     */
-    private $logFileName = 'connector.log';
-
-    /**
      * @var \Magento\Framework\App\Filesystem\DirectoryList
      */
     private $directoryList;
@@ -71,15 +66,6 @@ class File
         // tab character
         $this->delimiter = ',';
         $this->enclosure = '"';
-
-        $logDir = $directoryList->getPath('log');
-        if (! is_dir($logDir)) {
-            mkdir($directoryList->getPath('var')  . DIRECTORY_SEPARATOR . 'log');
-        }
-        $writer = new \Zend\Log\Writer\Stream($logDir . DIRECTORY_SEPARATOR .  $this->logFileName);
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $this->connectorLogger  = $logger;
     }
 
     /**
@@ -323,27 +309,6 @@ class File
         } catch (\Exception $e) {
             return $e->getMessage() . $pathLogfile;
         }
-    }
-
-    /**
-     * @param string|array|mixed $data
-     *
-     * @return null
-     */
-    public function info($data)
-    {
-        $this->connectorLogger->info($data);
-    }
-
-    /**
-     * @param string $message
-     * @param array $extra
-     *
-     * @return null
-     */
-    public function debug($message, $extra)
-    {
-        $this->connectorLogger->debug($message, $extra);
     }
 
     /**
