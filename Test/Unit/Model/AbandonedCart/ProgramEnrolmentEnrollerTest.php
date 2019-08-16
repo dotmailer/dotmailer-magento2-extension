cdec77ee91e80c6102806bd49641c5f05e2f7cff
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2019-08-16 13:37:32 +0000

    Merged PR 15599: Split quotes into smaller batches
    Unpredictable amount of quotes may be processed which may lead to system overload. Batch processing should be implemented.
    
    **What Changed**
    Now we split quotes in to smaller batches to avoid this.
    
    **How to test**
    Re run abandoned cart Program Enrollment and make sure that works as expected
    
    Related work items: #93867

--- Test/Unit/Model/AbandonedCart/ProgramEnrolmentEnrollerTest.php
+++ Test/Unit/Model/AbandonedCart/ProgramEnrolmentEnrollerTest.php
@@ -2,6 +2,7 @@
 
 namespace Dotdigitalgroup\Email\Test\Unit\Model\AbandonedCart\ProgramEnrolment;
 
+use Dotdigitalgroup\Email\Helper\Config;
 use Dotdigitalgroup\Email\Model\AbandonedCart\ProgramEnrolment\Enroller;
 use Dotdigitalgroup\Email\Model\AbandonedCart\ProgramEnrolment\Interval;
 use Dotdigitalgroup\Email\Model\AbandonedCart\ProgramEnrolment\Saver;
@@ -10,6 +11,7 @@ use Dotdigitalgroup\Email\Model\ResourceModel\Order\CollectionFactory;
 use Dotdigitalgroup\Email\Model\ResourceModel\Order\Collection;
 use Dotdigitalgroup\Email\Helper\Data;
 use Dotdigitalgroup\Email\Model\AbandonedCart\CartInsight\Data as CartInsight;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
 class ProgramEnrolmentEnrollerTest extends TestCase
@@ -59,20 +61,31 @@ class ProgramEnrolmentEnrollerTest extends TestCase
      */
     private $cartInsight;
 
+    /**
+     * @var ObjectManager
+     */
+    protected $objectManager;
+
     protected function setUp()
     {
         $this->orderCollectionFactoryMock = $this->getMockBuilder(CollectionFactory::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $this->orderCollectionMock = $this->createMock(Collection::class);
+
+
         $this->dataHelperMock = $this->getMockBuilder(Data::class)
             ->disableOriginalConstructor()
             ->getMock();
+
         $this->scopeConfigModelMock = $this->createMock(\Magento\Framework\App\Config\ScopeConfigInterface::class);
         $this->interval = $this->createMock(Interval::class);
         $this->saver = $this->createMock(Saver::class);
         $this->rules = $this->createMock(Rules::class);
         $this->cartInsight = $this->createMock(CartInsight::class);
+
+        //We need to mock an Object to behave as a Collection in order to pass the tests
+        $this->objectManager = new ObjectManager($this);
+        $this->orderCollectionMock = $this->objectManager->getCollectionMock(Collection::class, [$this->orderCollectionMock]);
 
         $this->model = new Enroller(
             $this->orderCollectionFactoryMock,
@@ -111,7 +124,7 @@ class ProgramEnrolmentEnrollerTest extends TestCase
             ->method('getScopeConfig')
             ->willReturn($this->scopeConfigModelMock);
 
-        $this->scopeConfigModelMock->expects($this->atLeastOnce())
+        $this->scopeConfigModelMock->expects($this->at(0))
             ->method('getValue')
             ->with(
                 \Dotdigitalgroup\Email\Helper\Config::XML_PATH_LOSTBASKET_ENROL_TO_PROGRAM_ID,
@@ -120,31 +133,53 @@ class ProgramEnrolmentEnrollerTest extends TestCase
             )
             ->willReturn($programId);
 
+        $this->scopeConfigModelMock->expects($this->at(1))
+            ->method('getValue')
+            ->with(
+                Config::XML_PATH_CONNECTOR_SYNC_LIMIT,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            )
+            ->willReturn($batchSize = 500);
+
         // Enrolment interval
         $this->interval
             ->method('getAbandonedCartProgramEnrolmentWindow')
             ->with($storeId)
             ->willReturn($updated);
 
-        $this->orderCollectionFactoryMock
+        $this->orderCollectionFactoryMock->expects($this->atLeastOnce())
             ->method('create')
             ->willReturn($this->orderCollectionMock);
 
-        $quotesArray = [
-            $this->createMock(\Magento\Quote\Model\Quote::class)
-        ];
-
-        $this->orderCollectionMock->expects($this->once())
+        $this->orderCollectionMock->expects($this->atLeastOnce())
             ->method('getStoreQuotesForGuestsAndCustomers')
             ->with(
                 $storeId,
                 $updated
             )
-            ->willReturn($quotesArray);
+            ->willReturn($this->orderCollectionMock);
+
+        $this->orderCollectionMock ->expects($this->atLeastOnce())
+            ->method('getSize')
+            ->willReturn(1500);
+
+        $this->orderCollectionMock->expects($this->atLeastOnce())
+            ->method('setPageSize')
+            ->willReturn($this->orderCollectionMock);
+
+        $this->orderCollectionMock->expects($this->atLeastOnce())
+            ->method('setCurPage')
+            ->willReturn($this->orderCollectionMock);
+
+        $this->saver->expects($this->atLeastOnce())
+            ->method('save');
+
+        $this->cartInsight->expects($this->atLeastOnce())
+            ->method('send');
 
         $this->rules
             ->method('apply')
-            ->with($quotesArray, $storeId);
+            ->with($this->orderCollectionMock , $storeId);
 
         $this->model->process();
     }
