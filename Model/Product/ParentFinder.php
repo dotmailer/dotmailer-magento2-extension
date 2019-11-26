e9498802093372d605d94ffb5607b0a7dbf74722
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2019-11-26 12:04:24 +0000

    Merged PR 17743: Product Catalog Schema updated to display parent item ids
    Product Catalog Schema updated to display parent item Ids
    
    **How to review**
    
    - Check out to this branch
    - Run Catalog Sync
    - Make sure that if a synced product has a **parent** then in json schema we display the `parent_id` and `type` changed to "Variant"
    - Ensure that `parent_id` is always visible in the schema
    
    **What changed**
    - a new private function added to Product class to get any parent id if exists
    - UrlFinder refactored. Now a new ParentFinder class created which is responsible ONLY to fetch the parent Product of a product (if exists).
    - ParentFinder is being use comonly by Connector\Product and Catalog\UrlFinder.
    - UrlFinder is responsible ONLY to fetch product urls and not to searching for Parent Products as well
    - Unit Tests updated
    
    Related work items: #101741

---
+++ Model/Product/ParentFinder.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Model\Product;
+
+use Dotdigitalgroup\Email\Helper\Data;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+
+class ParentFinder
+{
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var Data
+     */
+    private $helper;
+
+    /**
+     * @var \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable
+     */
+    private $configurableType;
+
+    /**
+     * @var \Magento\GroupedProduct\Model\Product\Type\Grouped
+     */
+    private $groupedType;
+
+    /**
+     * @var \Magento\Bundle\Model\ResourceModel\Selection
+     */
+    private $bundleSelection;
+
+    /**
+     * ParentFinder constructor.
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     * @param Data $helper
+     * @param \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $configurableType
+     * @param \Magento\GroupedProduct\Model\Product\Type\Grouped $groupedType
+     * @param \Magento\Bundle\Model\ResourceModel\Selection $bundleSelection
+     */
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        Data $helper,
+        \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $configurableType,
+        \Magento\GroupedProduct\Model\Product\Type\Grouped $groupedType,
+        \Magento\Bundle\Model\ResourceModel\Selection $bundleSelection
+    ) {
+        $this->productRepository = $productRepository;
+        $this->helper = $helper;
+        $this->configurableType = $configurableType;
+        $this->groupedType = $groupedType;
+        $this->bundleSelection = $bundleSelection;
+    }
+
+    /**
+     * @param $product
+     * @return \Magento\Catalog\Api\Data\ProductInterface|null
+     */
+    public function getParentProduct($product)
+    {
+        try {
+            if ($parentId = $this->getFirstParentId($product)) {
+                return $this->productRepository->getById($parentId, false, $product->getStoreId());
+            }
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+            $this->helper->debug(
+                $e->getMessage() . ' Parent Product: ' .
+                $parentId . ', 
+                Child Product: ' . $product->getId()
+            );
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $product
+     * @return int|null
+     */
+    public function getProductParentIdToCatalogSync($product)
+    {
+        $parent = $this->getParentProduct($product);
+
+        if ($parent && $parent->getTypeId() === Configurable::TYPE_CODE) {
+            return $parent->getId();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $product
+     * @return string|null
+     */
+    private function getFirstParentId($product)
+    {
+        $configurableProducts = $this->configurableType->getParentIdsByChild($product->getId());
+        if (isset($configurableProducts[0])) {
+            return $configurableProducts[0];
+        }
+
+        $groupedProducts = $this->groupedType->getParentIdsByChild($product->getId());
+        if (isset($groupedProducts[0])) {
+            return $groupedProducts[0];
+        }
+
+        $bundleProducts = $this->bundleSelection->getParentIdsByChild($product->getId());
+        if (isset($bundleProducts[0])) {
+            return $bundleProducts[0];
+        }
+
+        return null;
+    }
+}
---
+++ Test/Unit/Model/Product/ParentFinderTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Test\Unit\Model\Product;
+
+use Dotdigitalgroup\Email\Helper\Data;
+use Dotdigitalgroup\Email\Model\Product\ParentFinder;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
+use Magento\GroupedProduct\Model\Product\Type\Grouped;
+use Magento\Bundle\Model\ResourceModel\Selection;
+use PHPUnit\Framework\TestCase;
+
+
+class ParentFinderTest extends TestCase
+{
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $productRepositoryMock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $helperMock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $configurableTypeMock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $groupedTypeMock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $bundleSelectionMock;
+    /**
+     * @var ParentFinder
+     */
+    private $parentFinder;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $productMock;
+
+    protected function setUp()
+    {
+        $this->productRepositoryMock = $this->createMock(ProductRepositoryInterface::class);
+        $this->helperMock = $this->createMock(Data::class);
+        $this->configurableTypeMock = $this->createMock(Configurable::class);
+        $this->groupedTypeMock = $this->createMock(Grouped::class);
+        $this->bundleSelectionMock = $this->createMock(Selection::class);
+        $this->productMock = $this->createMock(Product::class);
+
+
+        $this->parentFinder = new ParentFinder(
+            $this->productRepositoryMock,
+            $this->helperMock,
+            $this->configurableTypeMock,
+            $this->groupedTypeMock,
+            $this->bundleSelectionMock
+        );
+    }
+
+    public function testFetchForSimpleNotVisibleProductWithConfigurableTypeParent()
+    {
+
+        $this->configurableTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn([10]);
+
+        $this->groupedTypeMock->expects($this->never())
+            ->method('getParentIdsByChild');
+
+        $this->bundleSelectionMock->expects($this->never())
+            ->method('getParentIdsByChild');
+
+        $this->buildAssertions();
+    }
+
+    public function testFetchForSimpleNotVisibleProductWithGroupedTypeParent()
+    {
+        $this->configurableTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->groupedTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn([10]);
+
+        $this->bundleSelectionMock->expects($this->never())
+            ->method('getParentIdsByChild');
+
+        $this->buildAssertions();
+    }
+
+    public function testFetchForSimpleNotVisibleProductWithBundleTypeParent()
+    {
+        $this->configurableTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->groupedTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->bundleSelectionMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn([10]);
+
+        $this->buildAssertions();
+    }
+
+    public function testFetchForSimpleNotVisibleProductWithNoParent()
+    {
+        $this->configurableTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->groupedTypeMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->bundleSelectionMock->expects($this->once())
+            ->method('getParentIdsByChild')
+            ->with($this->productMock->getId())
+            ->willReturn(null);
+
+        $this->parentFinder->getParentProduct($this->productMock);
+    }
+
+    /**
+     * Builds all the mutual assertions for all cases
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function buildAssertions()
+    {
+
+        // New Product mock for parent
+        $parentProduct = $this->createMock(Product::class);
+
+        $this->productRepositoryMock->expects($this->once())
+            ->method('getById')
+            ->with(10)
+            ->willReturn($parentProduct);
+
+        $this->parentFinder->getParentProduct($this->productMock);
+    }
+}
