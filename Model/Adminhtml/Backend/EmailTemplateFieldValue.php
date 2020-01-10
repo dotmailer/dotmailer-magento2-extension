69523d412b419ddc45afb7efc8b5032561b1c29c
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2020-01-10 10:47:12 +0000

    Merged PR 18345: Function added to check if template is mapped
    This bug happens when we try to save EC > Email Templates section for FIRST TIME ONLY after we install the connector.
    
    **How to reproduce**
    - Manually delete all the records from `core_config_data`  with path `dotmailer_email_templates/email_templates/*`
    
    - Go to Configuration > Customers > Customer Configuration > Create New Account Options
    and set a new Default Welcome Email template
    
    - Go to EC > Email Templates (make sure that all drop down fields and especially the "New Account" field is set to `please select`
    
    - Click Save
    
    - Go back to Configuration > Customers > Customer Configuration > Create New Account Options and make sure that the Default Welcome Email template is still there and not overriden by default.
    
    Related work items: #100076

--- Model/Adminhtml/Backend/EmailTemplateFieldValue.php
+++ Model/Adminhtml/Backend/EmailTemplateFieldValue.php
@@ -114,19 +114,43 @@ class EmailTemplateFieldValue extends \Magento\Framework\App\Config\Value
                 );
             }
         } else {
-            //remove the config for core email template
-            $this->helper->deleteConfigData(
-                $dotTemplate->templateConfigMapping[$templateConfigId],
-                $scope,
-                $scopeId
-            );
-            //remove the config for dotmailer template
-            $this->helper->deleteConfigData(
-                $dotTemplate->templateConfigIdToDotmailerConfigPath[$templateConfigId],
-                $scope,
-                $scopeId
-            );
+            if (!$this->isFirstTimeMapped($dotTemplate, $templateConfigId, $scope, $scopeId) && $this->getOldValue()) {
+                //remove the config for core email template
+                $this->helper->deleteConfigData(
+                    $dotTemplate->templateConfigMapping[$templateConfigId],
+                    $scope,
+                    $scopeId
+                );
+                //remove the config for dotmailer template
+                $this->helper->deleteConfigData(
+                    $dotTemplate->templateConfigIdToDotmailerConfigPath[$templateConfigId],
+                    $scope,
+                    $scopeId
+                );
+            }
         }
         return parent::beforeSave();
+    }
+
+    /**
+     * @param $dotTemplate
+     * @param $templateConfigId
+     * @param $scope
+     * @param $scopeId
+     * @return bool
+     */
+    private function isFirstTimeMapped($dotTemplate, $templateConfigId, $scope, $scopeId)
+    {
+        $configValue =  $this->helper->getConfigValue(
+            $dotTemplate->templateConfigMapping[$templateConfigId],
+            $scope,
+            $scopeId
+        );
+
+        if (is_numeric($configValue) && !empty($configValue)) {
+            return true;
+        }
+
+        return false;
     }
 }
