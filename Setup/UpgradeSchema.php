12acd814394b5f4f5dc7a694fe9c66fc6483cf9b
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2020-01-30 13:41:32 +0000

    Merged PR 18933: Remove PHP unserialize() from upgrade script
    ## What's being changed
    
    The upgrade script for merchants on < 2.2.1 of our connector.
    
    ## Why it's being changed
    
    This PR removes the last remaining call to PHP's native unserialize() method, which is discouraged by Magento. It was still in place to convert M1-style serialized strings stored in several of our tables, into JSON - but this was an upgrade written for the 2.2.1 release in April 2017.
    
    Merchants who are still on older versions of our connector than 2.2.1 are advised to upgrade, then run our migration script to refresh the data in our `email_` prefixed tables.
    
    ```
    bin/magento dotdigital:migrate
    ```
    
    ## How to review / test this change
    
    - Set your connector version to < 2.2.1
    - Run setup:upgrade
    - Check that the remaining parts of the upgrade script for this version are run without error.
    
    Related work items: #101528

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -46,7 +46,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
         $setup->startSetup();
         $connection = $setup->getConnection();
 
-        $this->upgradeOneOneZeoToTwoTwoOne($setup, $context, $connection);
+        $this->upgradeOneOneZeroToTwoTwoOne($setup, $context, $connection);
         $this->upgradeTwoThreeSixToTwoFiveFour($setup, $context);
         $this->upgradeTwoFiveFourToThreeZeroThree($setup, $context);
         $this->upgradeThreeTwoTwo($setup, $context);
@@ -126,126 +126,6 @@ class UpgradeSchema implements UpgradeSchemaInterface
      *
      * @return null
      */
-    private function convertDataForConfig(SchemaSetupInterface $setup, $connection)
-    {
-        $configTable = $setup->getTable('core_config_data');
-        //customer and order custom attributes from config
-        $select = $connection->select()->from(
-            $configTable
-        )->where(
-            'path IN (?)',
-            [
-                'connector_automation/order_status_automation/program',
-                'connector_data_mapping/customer_data/custom_attributes'
-            ]
-        );
-        $rows = $setup->getConnection()->fetchAssoc($select);
-
-        $serializedRows = array_filter($rows, function ($row) {
-            return $this->isSerialized($row['value']);
-        });
-
-        foreach ($serializedRows as $id => $serializedRow) {
-            $convertedValue = $this->json->serialize($this->unserialize($serializedRow['value']));
-            $bind = ['value' => $convertedValue];
-            $where = [$connection->quoteIdentifier('config_id') . '=?' => $id];
-            $connection->update($configTable, $bind, $where);
-        }
-    }
-
-    /**
-     * @param SchemaSetupInterface $setup
-     * @param AdapterInterface $connection
-     *
-     * @return null
-     */
-    private function convertDataForRules(SchemaSetupInterface $setup, $connection)
-    {
-        $rulesTable = $setup->getTable(Schema::EMAIL_RULES_TABLE);
-        //rules data
-        $select = $connection->select()->from($rulesTable);
-        $rows = $setup->getConnection()->fetchAssoc($select);
-
-        $serializedRows = array_filter($rows, function ($row) {
-            return $this->isSerialized($row['conditions']);
-        });
-
-        foreach ($serializedRows as $id => $serializedRow) {
-            $convertedValue = $this->json->serialize($this->unserialize($serializedRow['conditions']));
-            $bind = ['conditions' => $convertedValue];
-            $where = [$connection->quoteIdentifier('id') . '=?' => $id];
-            $connection->update($rulesTable, $bind, $where);
-        }
-    }
-
-    /**
-     * @param SchemaSetupInterface $setup
-     * @param AdapterInterface $connection
-     *
-     * @return null
-     */
-    private function convertDataForImporter(SchemaSetupInterface $setup, $connection)
-    {
-        $importerTable = $setup->getTable(Schema::EMAIL_IMPORTER_TABLE);
-        //imports that are not imported and has TD data
-        $select = $connection->select()
-            ->from($importerTable)
-            ->where('import_status =?', 0)
-            ->where('import_type IN (?)', ['Catalog_Default', 'Orders' ])
-        ;
-        $rows = $setup->getConnection()->fetchAssoc($select);
-
-        $serializedRows = array_filter($rows, function ($row) {
-            return $this->isSerialized($row['import_data']);
-        });
-
-        foreach ($serializedRows as $id => $serializedRow) {
-            $convertedValue = $this->json->serialize($this->unserialize($serializedRow['import_data']));
-            $bind = ['import_data' => $convertedValue];
-            $where = [$connection->quoteIdentifier('id') . '=?' => $id];
-            $connection->update($importerTable, $bind, $where);
-        }
-    }
-
-    /**
-     * Check if value is a serialized string
-     *
-     * @param string $value
-     * @return boolean
-     */
-    private function isSerialized($value)
-    {
-        return (boolean) preg_match('/^((s|i|d|b|a|O|C):|N;)/', $value);
-    }
-
-    /**
-     * @param string $string
-     * @return mixed
-     */
-    private function unserialize($string)
-    {
-        if (false === $string || null === $string || '' === $string) {
-            throw new \InvalidArgumentException('Unable to unserialize value.');
-        }
-        set_error_handler(
-            function () {
-                restore_error_handler();
-                throw new \InvalidArgumentException('Unable to unserialize value, string is corrupted.');
-            },
-            E_NOTICE
-        );
-        $result = unserialize($string, ['allowed_classes' => false]);
-        restore_error_handler();
-
-        return $result;
-    }
-
-    /**
-     * @param SchemaSetupInterface $setup
-     * @param AdapterInterface $connection
-     *
-     * @return null
-     */
     private function addIndexKeyForCatalog(
         SchemaSetupInterface $setup,
         \Magento\Framework\DB\Adapter\AdapterInterface $connection
@@ -316,7 +196,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
      * @param SchemaSetupInterface $setup
      * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
      */
-    private function convertDataAndAddIndexes(SchemaSetupInterface $setup, $connection)
+    private function changeColumnAndAddIndexes(SchemaSetupInterface $setup, $connection)
     {
         //modify the condition column name for the email_rules table - reserved name for mysql
         $rulesTable = $setup->getTable(Schema::EMAIL_RULES_TABLE);
@@ -333,18 +213,6 @@ class UpgradeSchema implements UpgradeSchemaInterface
                 ]
             );
         }
-        /**
-         * Core config data.
-         */
-        $this->convertDataForConfig($setup, $connection);
-        /**
-         * Importer data.
-         */
-        $this->convertDataForImporter($setup, $connection);
-        /**
-         * Rules conditions.
-         */
-        $this->convertDataForRules($setup, $connection);
         /**
          * Index foreign key for email catalog.
          */
@@ -411,7 +279,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
      * @param ModuleContextInterface $context
      * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
      */
-    private function upgradeOneOneZeoToTwoTwoOne(
+    private function upgradeOneOneZeroToTwoTwoOne(
         SchemaSetupInterface $setup,
         ModuleContextInterface $context,
         $connection
@@ -426,10 +294,8 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if (version_compare($context->getVersion(), '2.1.0', '<')) {
             $this->addColumnToCouponTable($setup, $connection);
         }
-
-        //replace serialize with json_encode
         if (version_compare($context->getVersion(), '2.2.1', '<')) {
-            $this->convertDataAndAddIndexes($setup, $connection);
+            $this->changeColumnAndAddIndexes($setup, $connection);
         }
     }
 
