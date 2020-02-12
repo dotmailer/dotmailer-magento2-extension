ecd5c7aaaf51646ba4c2d053176463b3f733304f
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2020-02-12 14:41:47 +0000

    Merged PR 19359: Fix failing integration tests
    ## What's being changed
    
    Two integration test files.
    
    ## Why it's being changed
    
    These errors were not spotted when merging the Serializer class update PR.
    
    Related work items: #108123

--- Test/Integration/_files/Importer/create_multiple_email_importer_records.php
+++ Test/Integration/_files/Importer/create_multiple_email_importer_records.php
@@ -1,6 +1,6 @@
 <?php
 
-use Dotdigitalgroup\Email\Model\Config\Json;
+use Magento\Framework\Serialize\Serializer\Json;
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
@@ -9,12 +9,27 @@ $importerResourceModel = $objectManager->create(\Dotdigitalgroup\Email\Model\Res
 $serializer = $objectManager->create(Json::class);
 
 for ($i=0; $i<25; $i++) {
+    $importedData = $serializer->serialize([
+        "id" => $i,
+        "customerId" => $i + 1,
+        "email" => "chaz@kangeroo.com",
+        "productName" => "Chaz Kangeroo Hoodie",
+        "productSku" => "24-MB01",
+        "reviewDate" => "2019-11-14 10:38:21",
+        "websiteName" => "Main Website",
+        "storeName" => "Default Store View",
+        "Rating" => [
+            "ratingScore" => 1
+        ]
+    ]);
+
     $importerModel = $objectManager->create(Dotdigitalgroup\Email\Model\Importer::class);
 
     $importerModel->setImportType("Reviews");
     $importerModel->setWebsiteId(1);
     $importerModel->setImportStatus(0);
     $importerModel->setImportMode("Bulk");
+    $importerModel->setImportData($importedData);
 
     $importerResourceModel->save($importerModel);
 }
