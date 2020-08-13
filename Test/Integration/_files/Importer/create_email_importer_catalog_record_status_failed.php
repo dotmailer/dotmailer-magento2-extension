a08776067107a03a0f514bf6e18e7d1c83cb4fcb
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2020-08-13 09:45:03 +0000

    Merged PR 23650: Fix static tests for fixtures
    Check we have no static test error for duplicate code in integration tests fixtures
    
    Related work items: #122825

--- Test/Integration/_files/Importer/create_email_importer_catalog_record_status_failed.php
+++ Test/Integration/_files/Importer/create_email_importer_catalog_record_status_failed.php
@@ -4,24 +4,24 @@ use Magento\Framework\Serialize\Serializer\Json;
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
-$importerResourceModel = $objectManager->create(\Dotdigitalgroup\Email\Model\ResourceModel\Importer::class);
+$importerResourceModelForFailed = $objectManager->create(\Dotdigitalgroup\Email\Model\ResourceModel\Importer::class);
 
 $serializer = $objectManager->create(Json::class);
 
-$importedData = $serializer->serialize([
+$importedDataForFailed = $serializer->serialize([
     "id" => 1,
     "parent_id" => 2,
-    "name" => "Chaz Kangaroo_",
-    "sku" => "chaz-kangaroo_sku",
-    "price" => "25.0"
+    "name" => "Chaz Kangaroo2_",
+    "sku" => "chaz-kangaroo_sku2",
+    "price" => "125.0"
 ]);
 
-$importerModel = $objectManager->create(Dotdigitalgroup\Email\Model\Importer::class);
+$importerModelForFailed = $objectManager->create(Dotdigitalgroup\Email\Model\Importer::class);
 
-$importerModel->setImportType("Catalog_Default");
-$importerModel->setWebsiteId(0);
-$importerModel->setImportStatus(3);
-$importerModel->setImportMode("Bulk");
-$importerModel->setImportData($importedData);
+$importerModelForFailed->setImportType("Catalog_Default");
+$importerModelForFailed->setWebsiteId(0);
+$importerModelForFailed->setImportStatus(3);
+$importerModelForFailed->setImportMode("Bulk");
+$importerModelForFailed->setImportData($importedDataForFailed);
 
-$importerResourceModel->save($importerModel);
+$importerResourceModelForFailed->save($importerModelForFailed);
