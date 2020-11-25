6ca932f5134f6a9ba25b5f6d0f77bb4647bddb8c
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2020-11-25 17:46:15 +0000

    Merged PR 25888: Cache coupon block response
    ## What's being changed
    
    We've added caching to the coupon block, in the form of a class property.
    
    ## Why it's being changed
    
    Some merchants have experienced issues when generating coupons, repeatedly triggering the 'Already processed' exception in `DotdigitalCouponRequestProcessor`.
    
    Evidently in some setups, where we check for the response in the EDC controller:
    ```
    if (strlen($this->_view->getLayout()->getOutput()) < 10)
    ```
    the block is being requested and rendered a second time. In this case the `$couponGeneratorStatus` property is already set, and we throw an exception. Expected behaviour is that calling `getLayout()->getOutput()` should return the value for `ddg.coupon.container` from the `_renderElementCache` array in `Magento\Framework\View\Layout`.
    
    Adding the variable in the block ensures that this is returned _in case_ `_renderElementCache['ddg.coupon.container']` is not set, and the `Layout` class decides to render the element again.
    
    ## How to review / test this change
    
    - Hit a coupon EDC
    
    ## Notes
    
    We may want to scale this out to other EDCs.
    
    Related work items: #131205

--- Block/Coupon.php
+++ Block/Coupon.php
@@ -37,11 +37,16 @@ class Coupon extends \Magento\Framework\View\Element\Template
     private $font;
 
     /**
+     * @var string
+     */
+    private $couponCode;
+
+    /**
      * Coupon constructor.
      *
      * @param Context $context
      * @param Data $helper
-     * @param DotdigitalCouponGeneratorFactory $dotdigitalCouponGeneratorFactory
+     * @param DotdigitalCouponRequestProcessorFactory $dotdigitalCouponRequestProcessorFactory
      * @param Font $font
      * @param array $data
      */
@@ -62,13 +67,19 @@ class Coupon extends \Magento\Framework\View\Element\Template
      * Generates the coupon code based on the code id.
      *
      * @return string|null
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function generateCoupon()
     {
         try {
-            return $this->getCouponRequestProcessor()
+            // Protects against repeat generation
+            if (!empty($this->couponCode)) {
+                return $this->couponCode;
+            }
+            $this->couponCode = $this->getCouponRequestProcessor()
                 ->processCouponRequest($this->getRequest()->getParams())
                 ->getCouponCode();
+            return $this->couponCode;
         } catch (\ErrorException $e) {
             $this->helper->debug('Problem generating coupon', [
                 'message' => $e->getMessage(),
