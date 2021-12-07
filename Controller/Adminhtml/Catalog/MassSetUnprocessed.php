8ef6a5dd9fbb7b81c861ce73305de12c98bd5058
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2021-12-07 10:04:11 +0000

    Merged PR 34231: Add bulk actions to set products as processed / unprocessed for catalog sync
    ## What's being changed
    
    We've added MassActions in the catalog report to bulk set products as processed or unprocessed. Setting all products to unprocessed is equivalent to a catalog reset.
    
    ## Why it's being changed
    
    This accompanies the forthcoming improvements to catalog sync. If we encounter problems exporting one particular product in a batch, we'll skip it, log the exception, but continue batching. The product will be marked as processed, but will be referenced by id in the logs, so now we can manually mark this product as 'Not processed' so it will be re-synced.
    
    There are other use cases of course - this removes the need to 'edit' a product in order to have it re-sync. It also allows admins to mark whole chunks of their catalog as processed and not sync these items, which can be useful if these products have some sort of known data issue.
    
    ## How to review / test this change
    
    - Select one item from the Catalog report
    - Change its status from Processed to Not Processed
    - Repeat with a page of items, and with the entire collection
    
    Related work items: #162006

---
+++ Controller/Adminhtml/Catalog/MassSetUnprocessed.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Dotdigitalgroup\Email\Controller\Adminhtml\Catalog;
+
+use Dotdigitalgroup\Email\Model\ResourceModel\Catalog;
+use Dotdigitalgroup\Email\Model\ResourceModel\Catalog\Collection as CatalogCollection;
+use Dotdigitalgroup\Email\Model\ResourceModel\Catalog\CollectionFactory;
+use Magento\Framework\App\Request\Http as HttpRequest;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Ui\Component\MassAction\Filter;
+
+class MassSetUnprocessed extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Dotdigitalgroup_Email::catalog';
+
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * @var Catalog
+     */
+    private $collectionResource;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * MassSetUnprocessed constructor.
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param Filter $filter
+     * @param Catalog $collectionResource
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        Filter $filter,
+        Catalog $collectionResource,
+        CollectionFactory $collectionFactory
+    ) {
+        $this->filter = $filter;
+        $this->collectionResource = $collectionResource;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Redirect|\Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * @throws \Magento\Framework\Exception\NotFoundException|\Magento\Framework\Exception\LocalizedException
+     */
+    public function execute()
+    {
+        if (!$this->isPost($this->getRequest())) {
+            throw new \Magento\Framework\Exception\NotFoundException(__('Page not found.'));
+        }
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        /** @var CatalogCollection $collection */
+        $this->collectionResource->setUnprocessedByIds($collection->getAllProductIds());
+
+        $this->messageManager->addSuccessMessage(
+            __(
+                'A total of %1 record(s) have been set as not processed.',
+                $collectionSize
+            )
+        );
+
+        return $resultRedirect->setPath('*/*/');
+    }
+
+    /**
+     * @param RequestInterface $request
+     *
+     * @return bool
+     */
+    private function isPost(RequestInterface $request)
+    {
+        /** @var HttpRequest $request */
+        return $request->isPost();
+    }
+}
