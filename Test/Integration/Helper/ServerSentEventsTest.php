867272a5538119ed4fcd314d1b825e9823ac264c
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2022-08-15 15:54:22 +0000

    Merged PR 39201: Fix unit test flagged as risky
    ## What's being changed
    
    The ServerSentEventTest unit test.
    
    ## Why it's being changed
    
    The amended test opens a stream and checks various properties of the stream (it's actually an integration test since it doesn't use mocks). It generated a warning:
    ```
    1) Dotdigitalgroup\Email\Test\Unit\ServerSentEvent\ServerSentEventTest::testCreateResponse
    Test code or tested code did not (only) close its own output buffers
    
    OK, but incomplete, skipped, or risky tests!
    ```
    
    PHPUnit throws this error if the output buffer level at the end of the test does not match the level at the start. This PR removes the warning by restoring the output buffer level to 1.
    
    ## How to review / test this change
    
    - See green (as in really green) build.
    
    Related work items: #183101

--- Test/Integration/Helper/ServerSentEventsTest.php
+++ Test/Integration/Helper/ServerSentEventsTest.php
@@ -1,21 +1,19 @@
 <?php
 
-namespace Dotdigitalgroup\Email\Test\Unit\ServerSentEvent;
+namespace Dotdigitalgroup\Email\Test\Integration\Helper;
 
 use Dotdigitalgroup\Email\Helper\ServerSentEvents;
 use Dotdigitalgroup\Email\Logger\Logger;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 
-class ServerSentEventTest extends TestCase
+class ServerSentEventsTest extends TestCase
 {
-
     public function testConstructor()
     {
         $logger = new Logger('tests');
         $init_request = Request::create('/', 'GET', [], [], [], []);
-        $sse = new ServerSentEvents($init_request,$logger);
+        $sse = new ServerSentEvents($init_request, $logger);
         $this->assertEquals(true, $sse->client_reconnect);
         $this->assertEquals(false, $sse->allow_cors);
         $this->assertEquals(false, $sse->is_reconnect);
@@ -26,10 +24,9 @@ class ServerSentEventTest extends TestCase
      */
     public function testCreateResponse()
     {
-
         $logger = new Logger('tests');
         $init_request = Request::create('/', 'GET', [], [], [], []);
-        $sse = new ServerSentEvents($init_request,$logger);
+        $sse = new ServerSentEvents($init_request, $logger);
         $response = $sse->createResponse();
 
         $this->assertInstanceOf('\Symfony\Component\HttpFoundation\StreamedResponse', $response);
@@ -41,15 +38,21 @@ class ServerSentEventTest extends TestCase
         $this->assertEquals('*', $response->headers->get('Access-Control-Allow-Origin'));
         $this->assertEquals('true', $response->headers->get('Access-Control-Allow-Credentials'));
 
+        /**
+         * To prevent the warning:
+         * Test code or tested code did not (only) close its own output buffers.
+         *
+         * ob_get_level() returns 1 at the start of the test, and must be 1 at the end.
+         */
+        ob_start();
     }
 
     public function testGetEventListener()
     {
         $logger = new Logger('tests');
         $init_request = Request::create('/', 'GET', [], [], [], []);
-        $sse = new ServerSentEvents($init_request,$logger);
+        $sse = new ServerSentEvents($init_request, $logger);
         $this->assertEquals([], $sse->getEventHandlers());
     }
-
 }
 
