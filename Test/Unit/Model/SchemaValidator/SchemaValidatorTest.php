93597288c4cd8b29d4217ed05b3b88137c5e7488
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2023-02-03 12:36:54 +0000

    Merged PR 42909: Replace Zend_Uri with Magento framework URL validator
    ## What's being changed
    
    We replaced `Zend_Uri` with Magento's own validator for URL validation, in the code that checks insight data by type.
    
    ## Why it's being changed
    
    `Zend_Uri` is part of Zend Framework 1, which has been removed in Magento 2.4.6.
    
    ## How to review / test this change
    
    - Run catalog sync
    - Ensure no validation errors are reported in the logs
    - Ensure expected products are exported and queued with the importer
    - Sense check the importer JSON using jsonlint or similar
    - Hack the`$value` in the URL rule to test a 'bad' URL
    
    Related work items: #200327

--- Test/Unit/Model/SchemaValidator/SchemaValidatorTest.php
+++ Test/Unit/Model/SchemaValidator/SchemaValidatorTest.php
@@ -19,12 +19,12 @@ use Dotdigitalgroup\Email\Model\Validator\Schema\SchemaValidatorRule;
 use Dotdigitalgroup\Email\Model\Validator\Schema\SchemaValidatorRuleFactory;
 use Dotdigitalgroup\Email\Model\Validator\Schema\SchemaValidatorRuleSet;
 use Dotdigitalgroup\Email\Model\Validator\Schema\SchemaValidatorRuleSetFactory;
+use Magento\Framework\Url\Validator;
 use PHPUnit\Framework\TestCase;
 
 class SchemaValidatorTest extends TestCase
 {
-
-    public const ORDER_PATTERN = [
+    public const PATTERN = [
         'orderTotal' => ':isFloat',
         'currency' => ':isString',
         'purchaseDate' => ':dateFormat',
@@ -35,6 +35,7 @@ class SchemaValidatorTest extends TestCase
                 'price' => ':isFloat',
                 'sku' => ':isString',
                 'qty' => ':isInt',
+                'imagePath' => ':url',
             ]
         ]
     ];
@@ -50,11 +51,101 @@ class SchemaValidatorTest extends TestCase
     private $schemaOrderValidator;
 
     /**
+     * @var Validator|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $urlValidatorMock;
+
+    /**
+     * Prepare test for run
+     *
+     * @return void
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
+     */
+    protected function setUp() :void
+    {
+        $this->urlValidatorMock = $this->createMock(Validator::class);
+
+        $this->setUpValidator();
+
+        $this->schemaOrderValidator = new SchemaValidator(
+            $this->schemaOrderValidatorRuleSetFactory,
+            static::PATTERN
+        );
+    }
+
+    /**
+     * Test Valid Order
+     *
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
+     */
+    public function testPatternMatchIsValid()
+    {
+        $this->urlValidatorMock->expects($this->once())
+            ->method('isValid')
+            ->willReturn(true);
+
+        $this->schemaOrderValidator->isValid([
+            'orderTotal' => 2.12,
+            'currency' => 'USD',
+            'purchaseDate' => '2022-05-20 14:43:11',
+            'orderSubtotal' => 3.66,
+            'products' => [
+                [
+                    'name' => 'mock_product_name',
+                    'price' => 0.45,
+                    'sku' => 'mock_product_name',
+                    'qty' => (int) 5,
+                    'imagePath' => 'http://chaz.net/cdn/images/product/my-pic.jpg'
+                ]
+            ]
+        ]);
+        $this->assertEmpty($this->schemaOrderValidator->getErrors());
+    }
+
+    /**
+     * Test Valid Order
+     *
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
+     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
+     */
+    public function testPatternMatchIsNotValid()
+    {
+        $this->urlValidatorMock->expects($this->once())
+            ->method('isValid')
+            ->willReturn(false);
+
+        $this->schemaOrderValidator->isValid([
+            'orderTotal' => "2.12",
+            'currency' => null,
+            'purchaseDate' => '2022-05-20 14:43:11',
+            'orderSubtotal' => "3.66",
+            'products' => [
+                [
+                    'name' => 'mock_product_name',
+                    'price' => "0.45",
+                    'sku' => 'mock_product_name',
+                    'qty' => "5",
+                    'imagePath' => 'chaz/cdn/images/product/my-pic.jpg'
+                ]
+            ]
+        ]);
+        $this->assertNotEmpty($this->schemaOrderValidator->getErrors());
+        $this->assertIsArray($this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('orderTotal', $this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('orderSubtotal', $this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('currency', $this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('products.*.price', $this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('products.*.qty', $this->schemaOrderValidator->getErrors());
+        $this->assertArrayHasKey('products.*.imagePath', $this->schemaOrderValidator->getErrors());
+    }
+
+    /**
      * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
      */
     private function getRuleFactory($rule)
     {
-
         $dateFormatRuleFactory = $this->createMock(DateFormatRuleFactory::class);
         $dateFormatRuleFactory
             ->method('create')
@@ -76,9 +167,9 @@ class SchemaValidatorTest extends TestCase
             ->method('create')
             ->willReturn(new RequiredRule());
         $urlRuleFactory = $this->createMock(UrlRuleFactory::class);
-        $requiredRuleFactory
+        $urlRuleFactory
             ->method('create')
-            ->willReturn(new UrlRule());
+            ->willReturn(new UrlRule($this->urlValidatorMock));
 
         return new SchemaValidatorRule(
             $dateFormatRuleFactory,
@@ -92,15 +183,15 @@ class SchemaValidatorTest extends TestCase
     }
 
     /**
-     * Prepare test classes for Order validation
+     * Prepare test classes for validation
      *
      * @return void
      */
-    private function setUpOrderValidator()
+    private function setUpValidator()
     {
         $this->schemaOrderValidatorRuleSetFactory = $this->createMock(SchemaValidatorRuleSetFactory::class);
         $schemaOrderValidatorRuleMockFactory = $this->createMock(SchemaValidatorRuleFactory::class);
-        $schemaOrderValidatorRuleMockFactory->expects($this->exactly(8))
+        $schemaOrderValidatorRuleMockFactory->expects($this->exactly(9))
             ->method('create')
             ->withConsecutive(
                 [["pattern" =>'isFloat']],
@@ -110,7 +201,8 @@ class SchemaValidatorTest extends TestCase
                 [["pattern" =>'isString']],
                 [["pattern" =>'isFloat']],
                 [["pattern" =>'isString']],
-                [["pattern" =>'isInt']]
+                [["pattern" =>'isInt']],
+                [["pattern" =>'url']]
             )
             ->will(
                 $this->onConsecutiveCalls(
@@ -121,10 +213,11 @@ class SchemaValidatorTest extends TestCase
                     $this->getRuleFactory('isString'),
                     $this->getRuleFactory('isFloat'),
                     $this->getRuleFactory('isString'),
-                    $this->getRuleFactory('isInt')
+                    $this->getRuleFactory('isInt'),
+                    $this->getRuleFactory('url')
                 )
             );
-        $this->schemaOrderValidatorRuleSetFactory->expects($this->exactly(10))
+        $this->schemaOrderValidatorRuleSetFactory->expects($this->exactly(11))
             ->method('create')
             ->withConsecutive(
                 [["key" =>'orderTotal']],
@@ -136,10 +229,12 @@ class SchemaValidatorTest extends TestCase
                 [["key" =>'products.*.name']],
                 [["key" =>'products.*.price']],
                 [["key" =>'products.*.sku']],
-                [["key" =>'products.*.qty']]
+                [["key" =>'products.*.qty']],
+                [["key" =>'products.*.imagePath']]
             )
             ->will(
                 $this->onConsecutiveCalls(
+                    new SchemaValidatorRuleSet($schemaOrderValidatorRuleMockFactory),
                     new SchemaValidatorRuleSet($schemaOrderValidatorRuleMockFactory),
                     new SchemaValidatorRuleSet($schemaOrderValidatorRuleMockFactory),
                     new SchemaValidatorRuleSet($schemaOrderValidatorRuleMockFactory),
@@ -152,78 +247,5 @@ class SchemaValidatorTest extends TestCase
                     new SchemaValidatorRuleSet($schemaOrderValidatorRuleMockFactory)
                 )
             );
-    }
-
-    /**
-     * Prepare test for run
-     *
-     * @return void
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
-     */
-    protected function setUp() :void
-    {
-
-        $this->setUpOrderValidator();
-        $this->schemaOrderValidator = new SchemaValidator(
-            $this->schemaOrderValidatorRuleSetFactory,
-            static::ORDER_PATTERN
-        );
-    }
-
-    /**
-     * Test Valid Order
-     *
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
-     */
-    public function testPatternMatchIsValid()
-    {
-        $this->schemaOrderValidator->isValid([
-            'orderTotal' => 2.12,
-            'currency' => 'USD',
-            'purchaseDate' => '2022-05-20 14:43:11',
-            'orderSubtotal' => 3.66,
-            'products' => [
-                [
-                    'name' => 'mock_product_name',
-                    'price' => 0.45,
-                    'sku' => 'mock_product_name',
-                    'qty' => (int) 5
-                ]
-            ]
-        ]);
-        $this->assertEmpty($this->schemaOrderValidator->getErrors());
-    }
-
-    /**
-     * Test Valid Order
-     *
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\RuleNotDefinedException
-     * @throws \Dotdigitalgroup\Email\Model\Validator\Schema\Exception\PatternInvalidException
-     */
-    public function testPatternMatchIsNotValid()
-    {
-        $this->schemaOrderValidator->isValid([
-            'orderTotal' => "2.12",
-            'currency' => null,
-            'purchaseDate' => '2022-05-20 14:43:11',
-            'orderSubtotal' => "3.66",
-            'products' => [
-                [
-                    'name' => 'mock_product_name',
-                    'price' => "0.45",
-                    'sku' => 'mock_product_name',
-                    'qty' => "5"
-                ]
-            ]
-        ]);
-        $this->assertNotEmpty($this->schemaOrderValidator->getErrors());
-        $this->assertIsArray($this->schemaOrderValidator->getErrors());
-        $this->assertArrayHasKey('orderTotal', $this->schemaOrderValidator->getErrors());
-        $this->assertArrayHasKey('orderSubtotal', $this->schemaOrderValidator->getErrors());
-        $this->assertArrayHasKey('currency', $this->schemaOrderValidator->getErrors());
-        $this->assertArrayHasKey('products.*.price', $this->schemaOrderValidator->getErrors());
-        $this->assertArrayHasKey('products.*.qty', $this->schemaOrderValidator->getErrors());
     }
 }
