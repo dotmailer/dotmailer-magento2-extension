9be144e8cb95846e5dc74808d8a2874ba2d84558
Paco S <paco.shum@gmail.com>
2023-02-27 13:07:00 +0000

    fix merging to dev branch

--- Model/Sync/Order/BatchProcessor.php
+++ Model/Sync/Order/BatchProcessor.php
@@ -6,7 +6,6 @@ use Dotdigitalgroup\Email\Logger\Logger;
 use Dotdigitalgroup\Email\Model\Catalog\UpdateCatalogBulk;
 use Dotdigitalgroup\Email\Model\Importer;
 use Dotdigitalgroup\Email\Model\ImporterFactory;
-use Dotdigitalgroup\Email\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
 use Dotdigitalgroup\Email\Model\ResourceModel\OrderFactory as OrderResourceFactory;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
 
