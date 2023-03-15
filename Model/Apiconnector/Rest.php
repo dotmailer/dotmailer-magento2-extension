193c1379e9fd3c4e91a5d8f7d24d335d3c022b1b
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2023-03-15 10:37:31 +0000

    Merged PR 43799: Restore SSL verification on API requests
    ## What's being changed
    
    We restore SSL verification on API requests as previously this was disabled.
    
    ## Why it's being changed
    
    This has been flagged by security team as a risk and also there is no reason to disable this.
    
    ## How to review / test this change
    
    - Quick check if your API requests work.
    
    Related work items: #204854

--- Model/Apiconnector/Rest.php
+++ Model/Apiconnector/Rest.php
@@ -470,8 +470,6 @@ class Rest
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt(
             $ch,
             CURLOPT_HTTPHEADER,
