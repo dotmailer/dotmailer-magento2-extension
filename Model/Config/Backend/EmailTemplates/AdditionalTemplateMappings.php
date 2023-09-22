d85e9d7ffaf50fbdf9b7be56ec3bfeb835aaba2b
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2023-09-22 15:52:19 +0000

    Merged PR 48027: Fix broken layout for transactional email
    ## What's being changed
    
    The AdditionalTemplateMappings backend model.
    
    ## Why it's being changed
    
    The dependency on `SerializerInterface` cannot be null because the parent class `Serialized` requires it.
    
    ## How to review / test this change
    
    - Go to Configuration > Transactional Emails
    - Confirm there are no display errors
    
    Related work items: #226364

--- Model/Config/Backend/EmailTemplates/AdditionalTemplateMappings.php
+++ Model/Config/Backend/EmailTemplates/AdditionalTemplateMappings.php
@@ -3,45 +3,48 @@
 namespace Dotdigitalgroup\Email\Model\Config\Backend\EmailTemplates;
 
 use Dotdigitalgroup\Email\Model\Config\Backend\ArraySerialized;
+use Dotdigitalgroup\Email\Model\Config\Backend\Serialized;
+use Dotdigitalgroup\Email\Model\Email\TemplateFactory;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+use Magento\Framework\Serialize\SerializerInterface;
 
 class AdditionalTemplateMappings extends ArraySerialized
 {
     /**
-     * @var \Dotdigitalgroup\Email\Model\Email\TemplateFactory
+     * @var TemplateFactory
      */
     private $templateFactory;
 
     /**
-     * @var \Dotdigitalgroup\Email\Helper\Data
-     */
-    private $helper;
-
-    /**
-     * @param \Dotdigitalgroup\Email\Model\Email\TemplateFactory $templateFactory
-     * @param \Dotdigitalgroup\Email\Helper\Data $helper
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
-     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
-     * @param \Magento\Framework\Serialize\SerializerInterface|null $serializer
+     * @param TemplateFactory $templateFactory
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param SerializerInterface $serializer
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
      * @param array $data
      */
     public function __construct(
-        \Dotdigitalgroup\Email\Model\Email\TemplateFactory $templateFactory,
-        \Dotdigitalgroup\Email\Helper\Data $helper,
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\App\Config\ScopeConfigInterface $config,
-        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        \Magento\Framework\Serialize\SerializerInterface $serializer = null,
+        TemplateFactory $templateFactory,
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        SerializerInterface $serializer,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
         array $data = []
     ) {
         $this->templateFactory = $templateFactory;
-        $this->helper = $helper;
+
         parent::__construct(
             $serializer,
             $context,
@@ -55,8 +58,10 @@ class AdditionalTemplateMappings extends ArraySerialized
     }
 
     /**
-     * @return \Dotdigitalgroup\Email\Model\Config\Backend\Serialized
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * Before save.
+     *
+     * @return Serialized
+     * @throws LocalizedException
      */
     public function beforeSave()
     {
