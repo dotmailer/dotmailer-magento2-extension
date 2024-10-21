0cdb68a3838a721fa0fdfefd991994b359a222d9
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2024-10-21 07:09:16 +0000

    Merged PR 51494: Surface hidden import settings to tune bulk sync behaviour
    ## What's being changed
    
    We've exposed all the batch size configs governing sync behaviour in the admin, in Dotdigital > Developer > Import Settings. We didn't want these to be visible in Reports > Dotdigital > Dashboard so we've removed all configs from the dashboard other than the actual yellow dashboard panel.
    
    ## Why it's being changed
    
    We want to enable merchants to more easily use and understand these options for tuning bulk syncs.
    
    ## How to review / test this change
    
    - Review the admin UI
    - Test updating and saving the values
    - Observe the configs are updated in core_config_data
    - Remove any import_settings configs from core_config_data, confirm the default values are as expected
    - Run Integration Insights sync and see the new values captured as expected
    
    Related work items: #215987

--- Block/Adminhtml/Dashboard.php
+++ Block/Adminhtml/Dashboard.php
@@ -9,7 +9,6 @@ namespace Dotdigitalgroup\Email\Block\Adminhtml;
  */
 class Dashboard extends \Magento\Backend\Block\Widget\Container
 {
-
     /**
      * @var string
      */
@@ -25,55 +24,5 @@ class Dashboard extends \Magento\Backend\Block\Widget\Container
         $this->_controller = 'adminhtml_dashboard';
         $this->_headerText = __('Dashboard');
         parent::_construct();
-
-        /**
-         * Contact sync.
-         */
-        $this->buttonList->add(
-            'contact_sync',
-            [
-                'label' => __('Run Contact Sync'),
-                'class' => 'primary'
-            ]
-        );
-
-        /**
-         * Importer sync.
-         */
-        $this->buttonList->add(
-            'importer_sync',
-            [
-                'label' => __('Run Importer Sync'),
-                'class' => 'primary'
-            ]
-        );
-    }
-
-    /**
-     * Get contact sync link.
-     *
-     * @return string
-     */
-    public function getContactSyncLink()
-    {
-        return $this->getUrl('dotdigitalgroup_email/run/contactsync');
-    }
-
-    /**
-     * Get importer link.
-     *
-     * @return string
-     */
-    public function getImporterLink()
-    {
-        $query = [
-            '_query' => [
-                'sync-type' => 'importer'
-            ]
-        ];
-        return $this->getUrl(
-            'dotdigitalgroup_email/run/sync',
-            $query
-        );
     }
 }
