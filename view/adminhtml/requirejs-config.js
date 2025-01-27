6bdc154ce5328c051db32e50a4ea247203127023
Gustaf Landman <Gustaf.Landman@dotdigital.com>
2025-01-27 15:50:26 +0000

    Merged PR 61137: Add SearchableSelect frontend model for automation program lists
    ## What's being changed
    Search capability added as frontend_model on program select inputs
    Select2 added as a dependency to enable search in select fields
    
    ## Why it's being changed
    
    To make the program lists searchable
    
    ## How to review / test this change
    Goto **Config->Dotdigital->Abandoned Carts**
    and check the new ui when choosing "Enrol abandoned cart to"
    
    ## Notes
    
    The frontend_model can be used with any select field in system.xml that uses a source_model by adding the following line:
    
    `<frontend_model>Dotdigitalgroup\Email\Block\Adminhtml\Config\SearchableSelect</frontend_model>`
    
    Related work items: #145011

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,5 +1,10 @@
 var config = {
     paths: {
         'eventsource': 'Dotdigitalgroup_Email/js/lib/eventsource/eventsource-polyfill.min'
+    },
+    map: {
+        '*': {
+            select2: 'Dotdigitalgroup_Email/js/lib/select2/select2.full'
+        }
     }
 };
