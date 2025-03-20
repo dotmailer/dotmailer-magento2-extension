1b66ed375b09be4d6c7c210fca48eaf558175857
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2025-03-20 10:09:44 +0000

    Merged PR 63548: Move Logger from inheritance to composition
    ## What's being changed
    We have refactored the `Dotdigitalgroup\Email\Logger\Logger` class from a Inheritance design to a composition design.
    
    We have added a new `Dotdigitalgroup\Email\Api\Logger\LoggerInterface` and preference respectfully to indicate that the logger will not change its structure  in the foreseeable future.
    
    ## Why it's being changed
    In Magento 2.4.8, monolog/monolog 3+ is bundled, meaning \Monolog\Logger is now marked as @final. We could simply skip the PHPStan error and carry on, but I felt it might be preferable to follow the Adobe docs  in this case.
    
    ## How to review / test this change
    - Run setup:upgrade
    - Run any sync, confirm logs are output as expected
    
    Related work items: #292288

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -2,9 +2,79 @@
 
 namespace Dotdigitalgroup\Email\Logger;
 
-/**
- * @phpstan-ignore class.extendsFinalByPhpDoc
- */
-class Logger extends \Monolog\Logger
+use Dotdigitalgroup\Email\Api\Logger\LoggerInterface;
+use Monolog\Handler\HandlerInterface;
+use Monolog\Logger as MonologLogger;
+use Psr\Log\LoggerTrait;
+
+class Logger implements LoggerInterface
 {
+    use LoggerTrait;
+
+    /**
+     * Exceptional occurrences that are not errors
+     *
+     * Examples: Use of deprecated APIs, poor use of an API,
+     * undesirable things that are not necessarily wrong.
+     */
+    public const WARNING = MonologLogger::WARNING;
+
+    /**
+     * Runtime errors
+     */
+    public const ERROR = MonologLogger::ERROR;
+
+    /**
+     * Detailed debug information
+     */
+    public const DEBUG = MonologLogger::DEBUG;
+
+    /**
+     * Interesting events
+     *
+     * Examples: User logs in, SQL logs.
+     */
+    public const INFO = MonologLogger::INFO;
+
+    /**
+     * @var MonologLogger
+     */
+    private $logger;
+
+    /**
+     * DotdigitalLogger constructor.
+     *
+     * @param string $name
+     * @param array $handlers
+     * @param array $processors
+     */
+    public function __construct(string $name, array $handlers = [], array $processors = [])
+    {
+        $this->logger = new MonologLogger($name, $handlers, $processors);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function log($level, $message, array $context = []): void
+    {
+        $this->logger->log($level, $message, $context);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addRecord($level, $message, array $context = []): void
+    {
+        $this->logger->addRecord($level, $message, $context);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function pushHandler(HandlerInterface $handler): self
+    {
+        $this->logger->pushHandler($handler);
+        return $this;
+    }
 }
