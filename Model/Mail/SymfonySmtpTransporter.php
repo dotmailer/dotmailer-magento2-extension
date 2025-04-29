664fe9d58257b31e79c68d34ce8b587f190e0deb
Alastair Mucklow <Alastair.Mucklow@dotdigital.com>
2025-04-29 08:48:54 +0000

    Merged PR 64580: Fix double encoding issue on translated Laminas messages
    ## What's being changed
    
    The way in which Symfony messages are constructed from a Laminas\Mail\Message.
    
    ## Why it's being changed
    
    This was a regression caused by this [fix in 4.31.0](https://github.com/dotmailer/dotmailer-magento2-extension/commit/fc3c4eb9bdcbc6e48c44e05729bbfc314b3f335e).
    
    There were two things wrong:
    1. The message arrived in inboxes malformed, because we were re-encoding body text that was already encoded. It's necessary to get the raw content from the part, and use that as the body.
    2. `Content-Type` and `Content-Transfer-Encoding` headers were duplicated, since we were adding them as headers to the message when they were already applied to the Symfony\Component\Mime\Part\TextPart. The headers set on the TextPart also by default indicated a type of 'text/plain'. These incorrect dupes probably weren't causing the malforming issue, but they were wrong and could have caused a problem.
    
    ## How to review / test this change
    
    - In 2.3.7, 2.4.x and 2.4.8, switch to this branch
    - Enable and configure Dotdigital SMTP
    - Map a template to a transactional email event in Magento
    - Trigger the email
    - Check the email in a client
    - (only do this once) Download email from the transactional email dashboard and check the following headers are set, have the correct values and are not duplicated:
    ```
    X-DM-Received: from localhost (localhost [redacted])
    X-DM-ReceivedDate: 2025-04-25 16:29:45
    X-Envelope-To: redacted@emailsim.io
    Date: Fri, 25 Apr 2025 16:29:42 +0000
    MIME-Version: 1.0
    Content-Type: text/html; charset="utf-8"
    Content-Transfer-Encoding: quoted-printable
    Content-Disposition: inline
    Subject: =?UTF-8?Q?Hell=C3=B6?=
    To: redacted@emailsim.io
    From: =?utf-8?Q?R=C3=BCdi?= <dotmailer53@r1.dotdigital-email.com>
    ```
    
    ## Notes
    Magento\Framework\Mail\Template\TransportBuilder::prepareMessage only sets one part on the message for the body content. I'm throwing an exception in the unlikely event that we can't get a raw content string.
    
    Related work items: #297210

--- Model/Mail/SymfonySmtpTransporter.php
+++ Model/Mail/SymfonySmtpTransporter.php
@@ -8,11 +8,12 @@ use Dotdigitalgroup\Email\Helper\Transactional;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Mail\EmailMessage;
 use Magento\Framework\Mail\EmailMessageInterface;
+use Magento\Framework\Mail\MimeInterface;
+use Magento\Framework\Mail\MimePart;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\Transport\Smtp\Auth\LoginAuthenticator;
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 use Symfony\Component\Mailer\Transport\TransportInterface as SymfonyTransportInterface;
-use Symfony\Component\Mime\Header\Headers;
 use Symfony\Component\Mime\Message as SymfonyMimeMessage;
 use Symfony\Component\Mime\Part\TextPart;
 
@@ -79,7 +80,7 @@ class SymfonySmtpTransporter
      * @param EmailMessageInterface $message
      *
      * @return SymfonyMimeMessage
-     * @throws \DateMalformedStringException
+     * @throws \DateMalformedStringException|LocalizedException
      */
     private function getOrConvertMimeMessage(EmailMessageInterface $message): SymfonyMimeMessage
     {
@@ -88,24 +89,38 @@ class SymfonySmtpTransporter
             return $message->getSymfonyMessage();
         }
 
-        $mimeMessage = $message->getMessageBody();
-        $messageHeaders = $message->getHeaders();
-
-        $symfonyHeaders = new Headers();
-        foreach ($messageHeaders as $headerName => $headerValue) {
-            if ($headerName === 'Date') {
-                $headerValue = new \DateTime($headerValue);
-            }
-            // required for symfony/mime 5.x, which we need to hold at for PHP 7.4 support
-            if (in_array($headerName, ['From', 'Reply-to', 'To', 'Cc', 'Bcc'], true)) {
-                $headerValue = [$headerValue];
-            }
-            $symfonyHeaders->addHeader($headerName, $headerValue);
+        $messageBody = '';
+        foreach ($message->getMessageBody()->getParts() as $part) {
+            $messageBody = $part->getRawContent();
+            break;
         }
 
-        $symfonyBody = new TextPart($mimeMessage->getMessage());
+        if (empty($messageBody)) {
+            throw new LocalizedException(__('Unable to get raw content from message parts'));
+        }
 
-        return new SymfonyMimeMessage($symfonyHeaders, $symfonyBody);
+        $symfonyBody = new TextPart(
+            $messageBody,
+            MimePart::CHARSET_UTF8,
+            'html',
+            MimeInterface::ENCODING_QUOTED_PRINTABLE
+        );
+        $symfonyBody->setDisposition('inline');
+        $symfonyMessage = new SymfonyMimeMessage(null, $symfonyBody);
+
+        foreach ($message->getHeaders() as $headerName => $headerValue) {
+            if ($headerName === 'Date') {
+                $symfonyMessage->getHeaders()->addDateHeader($headerName, new \DateTime($headerValue));
+            }
+            if ($headerName === 'Subject') {
+                $symfonyMessage->getHeaders()->addTextHeader('Subject', $headerValue);
+            }
+            if (in_array($headerName, ['From', 'Reply-to', 'To', 'Cc', 'Bcc'], true)) {
+                $symfonyMessage->getHeaders()->addMailboxListHeader($headerName, [$headerValue]);
+            }
+        }
+
+        return $symfonyMessage;
     }
 
     /**
